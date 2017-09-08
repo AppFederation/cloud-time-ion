f1b705eb53fbb690235ffe7e60418d8d82f19c8f
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2017-09-08 15:13:37 +0200

    Changed to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import {NodesService} from './nodes.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
