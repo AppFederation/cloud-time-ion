db4b6cf828fe0a53fe93e6344040eed44bc146a6
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2017-10-22 20:27:21 +0200

    script/config for deploy

---
+++ scripts/buildAndDeployToFirebase.sh
@@ -0,0 +1,15 @@
+
+set -x
+
+scriptDir="`dirname $0`"
+# $scriptDir/compileFirebaseRules.sh
+
+# TODO: add protractor.sh &&, once tests are reliable
+
+# To initialize, run: firebase use --add
+
+#   && cp -r assets dist \
+
+#ng build \
+ng build \
+  && firebase deploy
