db52525c40cd40956a931bdf3a4739387e338765
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2019-02-11 01:56:01 +0100

    MultiMap: check key validity, prevent duplicates (warning: O(n) complexity)

--- src/app/shared/multi-map.ts
+++ src/app/shared/multi-map.ts
@@ -1,13 +1,24 @@
+import { nullOrUndef } from './utils'
+
 export class MultiMap<K, V> {
   map = new Map<K, V[]>()
 
   add(key: K, val: V) {
+    this.checkKey(key)
     let coll = this.map.get(key)
     if ( ! coll ) {
       coll = []
       this.map.set(key, coll)
     }
-    coll.push(val)
+    if ( ! coll.includes(val) ) {
+      coll.push(val)
+    }
+  }
+
+  private checkKey(key: K) {
+    if (nullOrUndef(key)) {
+      throw new Error('Invalid key ' + key)
+    }
   }
 
   get(key: K) {
