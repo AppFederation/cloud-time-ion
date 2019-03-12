264cb6e856bd9151d837bdfc6bf7710d87d71a4b
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2019-03-13 00:34:09 +0100

    requestSignIn() prototype

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth'
+import { auth } from 'firebase/app';
+import { CoreModule } from './core.module'
+import * as firebase from 'firebase'
+
+export type UserId = string
 
 @Injectable({
-  providedIn: 'root' /* TODO: investigate https://medium.com/@tomastrajan/total-guide-to-angular-6-dependency-injection-providedin-vs-providers-85b7a347b59f ,
+  providedIn: CoreModule /* TODO: investigate https://medium.com/@tomastrajan/total-guide-to-angular-6-dependency-injection-providedin-vs-providers-85b7a347b59f ,
     https://stackoverflow.com/questions/50860898/angular-6-services-providedin-root-vs-coremodule */
 })
 export class AuthService {
 
-  constructor() { }
+  public userId: UserId
+
+  constructor(
+    // private angularFireAuth: AngularFireAuth,
+  ) {
+
+  }
 
   requestSignIn() {
-
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(result => {
+    // this.angularFireAuth.auth.signInWithPopup(new auth.GoogleAuthProvider()).then((result) => {
+      this.userId = result.user.uid
+      console.log('signInWithPopup', this.userId, result)
+    })
   }
 
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,12 +5,14 @@ import { CommandsService } from './commands.service'
 import { ClipboardService } from './clipboard.service'
 import {DbFirestoreModule} from '../db-firestore/db-firestore.module'
 import {TreeModelModule} from '../tree-model/tree-model.module'
+// import { AngularFireModule } from '@angular/fire'
 
 @NgModule({
   imports: [
     CommonModule,
     TreeModelModule,
     DbFirestoreModule,
+    // AngularFireModule, // AngularFireModule.initializeApp(environment.firebase)
   ],
   declarations: [],
   providers: [
