7665b3e6042df9a662c79242b5272947a907a36f
Karol <Depka>
2019-05-04 07:51:50 +0200

    AngularFireModule.initializeApp(config), AngularFireDatabaseModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AngularFireModule} from "@angular/fire";
+import {AngularFireDatabaseModule} from "@angular/fire/database";
+import {firebaseConfig} from "./firebase.config";
 
 @NgModule({
+
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/firebase.config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD8hiBc7WoQQISCDpDLMtiaakyKvKZwdkw",
+    authDomain: "stackblitzfire.firebaseapp.com",
+    databaseURL: "https://cloudtime-app.firebaseio.com",
+    storageBucket: "cloudtime-app.appspot.com",
+    // messagingSenderId: "42917465053"
+};
