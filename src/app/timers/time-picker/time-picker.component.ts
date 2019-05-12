011d4d4fb9fef0cfada3a4cbb3b3628c50051fcc
Karol Depka <karol-depka@users.noreply.github.com>
2019-05-12 22:51:46 +0200

    Prototype using PickerController for TimePickerComponent

--- src/app/timers/time-picker/time-picker.component.ts
+++ src/app/timers/time-picker/time-picker.component.ts
@@ -3,6 +3,7 @@ import {
   Input,
   OnInit, Output,
 } from '@angular/core';
+import {PickerController} from "@ionic/angular";
 
 @Component({
   selector: 'app-time-picker',
@@ -23,6 +24,7 @@ export class TimePickerComponent implements OnInit {
   totalTimeSeconds: number
 
   constructor(
+    public pickerController: PickerController,
       // public timersService: TimersService,
   ) { }
 
@@ -35,5 +37,50 @@ export class TimePickerComponent implements OnInit {
   onChangeTime() {
     this.totalTimeSeconds = this.pickerSeconds + 60 * this.pickerMinutes + 3600 * this.pickerHours
     this.durationSecondsChanged.emit(this.totalTimeSeconds)
+  }
+
+  async openTimePicker() {
+    let pickerElement = await this.pickerController.create({
+      buttons: [{
+        text: 'Done',
+      }],
+      columns: [
+        {
+          name: 'days',
+          options: [
+            {
+              text: '1',
+              value: 1
+            },
+            {
+              text: '2',
+              value: 2
+            },
+            {
+              text: '3',
+              value: 3
+            },
+          ]
+        },
+        {
+          name: 'years',
+          options: [
+            {
+              text: '1992',
+              value: 1992
+            },
+            {
+              text: '1993',
+              value: 1993
+            },
+            {
+              text: '1994',
+              value: 1994
+            },
+          ]
+        },
+      ]
+    });
+    await pickerElement.present()
   }
 }
