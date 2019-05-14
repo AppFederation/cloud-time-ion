e24527ad7b66fb7e4ab034854c80199eb38450b8
Karol Depka <karol-depka@users.noreply.github.com>
2019-05-14 20:20:57 +0200

    CloudTimeDroid

---
+++ CloudTimeDroid/app/src/main/java/com/innotopic/cloudtime/TimersService.kt
@@ -0,0 +1,11 @@
+package com.innotopic.cloudtime
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class TimersService: Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
