2480c2cf54f370a851c063e6bcc9d1e2b581280e
Karol Depka <karol-depka@users.noreply.github.com>
2019-05-16 02:44:44 +0200

    Issue #8: experiments with testcafe `<ion-fab-button id='fabAddTimer'`

--- tcafe/testSrc/timers.testc.ts
+++ tcafe/testSrc/timers.testc.ts
@@ -3,6 +3,19 @@ import {Selector} from 'testcafe';
 export function testTimers() {
   test(`Timers`, async t => {
     await t
+      // .wait(2000)
+      // .click('#fabAddTimer.hydrated')
+      // .wait(2000)
+      // .click('#fabAddTimer.hydrated')
+      // .wait(2000)
+      // .click('#fabAddTimer.hydrated')
+      // .click('#fabAddTimer.hydrated')
+      // // .click('#fabAddTimer.hydrated button')
+      // .wait(3000)
+      // .typeText("ion-icon", "TestCafe Timer " + new Date())
+      // .wait(5000)
+      // .click('#fabAddTimer.hydrated')
+      // .click(Selector(() => document.querySelector('#fabAddTimer2.hydrated').shadowRoot.querySelector('button')))
       .click('#fabAddTimer')
       .typeText("#timerTitle input", "TestCafe Timer " + new Date())
       .click('#hoursPicker #buttonPlus')
@@ -16,7 +29,7 @@ export function testTimers() {
       .click(Selector('.alert-button').withExactText("STOP"))
       .click('app-timer-details #buttonDeleteTimer')
       .click(Selector('.alert-button').withExactText("DELETE"))
-      .click('#fabAddTimer') // another one
+      .click('#fabAddTimer') // another timer
       .click('app-timer-details #buttonDeleteTimer')
       .click(Selector('.alert-button').withExactText("DELETE"))
   })
