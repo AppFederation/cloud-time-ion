82d87d2e6313d197fcc490c38818ace6a9498fee
Karol Depka <karol-depka@users.noreply.github.com>
2019-05-17 04:32:57 +0200

    Add timing and extract func in scripts/buildAndDeployToFirebase.sh

--- scripts/buildAndDeployToFirebase.sh
+++ scripts/buildAndDeployToFirebase.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-set -x
+doAll () {
+  echo Starting build and deploy `date`
+  set -x
 
-echo Starting build and deploy `date`
+  scriptDir="`dirname $0`"
+  # $scriptDir/compileFirebaseRules.sh
 
-scriptDir="`dirname $0`"
-# $scriptDir/compileFirebaseRules.sh
+  # TODO: add protractor.sh &&, once tests are reliable
 
-# TODO: add protractor.sh &&, once tests are reliable
+  # To initialize, run: firebase use --add
 
-# To initialize, run: firebase use --add
+  #   && cp -r assets dist \
 
-#   && cp -r assets dist \
+  #git tag test_`date --utc +%Y-%m-%d_%H.%M.%SZ`
 
-#git tag test_`date --utc +%Y-%m-%d_%H.%M.%SZ`
+  #ng build \
+  ionic build --prod --aot \
+    && firebase deploy --only hosting \
+    && git tag deploy_`date -u +%Y-%m-%d__%H.%M.%SZ` \
+    && git push --tags
 
-#ng build \
-ionic build --prod --aot \
-  && firebase deploy --only hosting \
-  && git tag deploy_`date -u +%Y-%m-%d__%H.%M.%SZ` \
-  && git push --tags
+  Echo Finished `date`
+}
 
-Echo Finished `date`
+time doAll
