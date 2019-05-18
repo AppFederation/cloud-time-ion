bf1c1af4dd0f1638a7f293837797c84be7d3d085
Karol Depka <karol-depka@users.noreply.github.com>
2019-05-18 09:06:00 +0200

    OdmService to fire localChanges$ on modified elements on incoming

--- src/app/odm/OdmService.ts
+++ src/app/odm/OdmService.ts
@@ -3,10 +3,13 @@ import {OdmBackend} from "./OdmBackend";
 import {OdmItem} from "./OdmItem";
 import {CachedSubject} from "../utils/CachedSubject";
 import {debugLog} from "../utils/log";
+import {OdmItemId} from "./OdmItemId";
 
 export abstract class OdmService<T extends OdmItem<T>> {
 
   throttleIntervalMs = 500
+  // TODO: throttleLocalUiMs = 200
+  // TODO: throttleSaveToDbMs = 1000 /* NOTE: this does NOT apply to things like start/stop timer which bypass throttle */
 
   odmBackendFactory = this.injector.get(OdmBackend)
   odmCollectionBackend = this.odmBackendFactory.createCollectionBackend(this.injector, this.className)
@@ -21,8 +24,29 @@ export abstract class OdmService<T extends OdmItem<T>> {
       let convertedCol = dbCol.map(dbItem => {
         return this.convertFromDbFormat(dbItem)
       });
+      const incomingNotKnownIfExisted = new Set<T>(convertedCol)
+      const incomingItemsWhichAreNew = new Set<T>(convertedCol)
+      const existingToDelete = new Set<T>(this.localItems$.lastVal)
+      for ( let incomingItemConverted of convertedCol ) {
+        let existingItem = this.getById(incomingItemConverted.id)
+        if ( existingItem ) {
+          existingToDelete.delete(existingItem) // it stays
+          Object.assign(existingItem, incomingItemConverted)
+          existingItem.localChanges$.next(existingItem)
+          incomingNotKnownIfExisted.delete(incomingItemConverted)
+        } else {
+          incomingItemsWhichAreNew.add(incomingItemConverted)
+        }
+      }
+      for ( let incomingItemToAdd of incomingItemsWhichAreNew) {
+        this.localItems$.lastVal.push(incomingItemToAdd)
+      }
+      // this.localItems$.lastVal.push(Array.from(incomingItemsWhichAreNew))
+
+      // FIXME: add new items to collection while leaving existing ones
       debugLog('localItems$.next')
-      this.localItems$.next(convertedCol)
+      // this.localItems$.next(convertedCol)
+      this.localItems$.next(this.localItems$.lastVal) // FIXME: some item might need to be deleted
     })
   }
 
@@ -36,5 +60,13 @@ export abstract class OdmService<T extends OdmItem<T>> {
 
   protected convertFromDbFormat(dbItem: T) {
     return dbItem // default impl
+  }
+
+  public getById(id: OdmItemId<T>) {
+    // this.getById()
+    return (
+      this.localItems$.lastVal &&
+      this.localItems$.lastVal.find(_ => _.id === id)
+    )
   }
 }
