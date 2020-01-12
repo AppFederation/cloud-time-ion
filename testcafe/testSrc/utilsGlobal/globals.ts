46618aa612c9bc1a728269670678fa5c224c7214
Karol Depka <karol-depka@users.noreply.github.com>
2020-01-12 01:23:48 +0100

    Fix failing TestCafe compilation

--- testcafe/testSrc/utilsGlobal/globals.ts
+++ testcafe/testSrc/utilsGlobal/globals.ts
@@ -2,6 +2,7 @@
 export const LOCALHOST_URL = 'http://localhost:4333/';
 // export const LOCALHOST_URL = 'http://localhost:8080/';
 
+export const APP_HTTP_API_URL = 'not-implemented:9999';
 
 export const TEST_USER = {
   userName: '',
