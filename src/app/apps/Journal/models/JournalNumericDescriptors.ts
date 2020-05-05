60225e1b4df498622f5faa46982395d1fdfad96d
Karol Depka <karol-depka@users.noreply.github.com>
2020-05-05 02:08:41 +0200

    Add JournalNumericDescriptors

--- src/app/apps/Journal/models/JournalNumericDescriptors.ts
+++ src/app/apps/Journal/models/JournalNumericDescriptors.ts
@@ -9,7 +9,7 @@ export class JournalNumericDescriptor extends UiFieldDef {
 
 }
 
-function jnd(anti?: string) {
+function jnd(anti?: string | { subTitle: string}) {
   return new JournalNumericDescriptor()
 }
 
@@ -21,21 +21,32 @@ export class JournalNumericDescriptors extends UiFieldDefs {
 
   static instance = new JournalNumericDescriptors()
 
+  importance = jnd({subTitle: `of this journal entry`})
   mood = jnd()
   hope = jnd()
   health = jnd()
   rest = jnd('tired')
+  'sleep quality' = jnd()
+  'sleep quantity' = jnd()
   relax = jnd('stressed')
   satisfaction = jnd('frustrated' /*?*/)
   productivity = jnd()
+  // work quality
+  // work quantity
   /** self-control? */
   discipline = jnd()
   cleanliness = jnd()
   grooming = jnd()
   order = jnd('chaos')
+  focus = jnd('distraction')
+  courage = jnd('fear') // / confidence ;; BUT courage is a kind of fearlessness even when lacking CONFIDENCE
+  outcome_independence = jnd()
+  confidence = jnd('doubts')
+  music = jnd()
   long_term_thinking = jnd()
   rationality = jnd()
   mindfulness = jnd()
+  alcohol = jnd()
 
   array = dictToArrayWithIds(this as any as Dict<JournalNumericDescriptor>)
 
