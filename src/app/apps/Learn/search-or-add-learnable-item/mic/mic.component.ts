46ae99ec27ddff40fba84a4a395ad2f5293d3a5f
Karol Depka <karol-depka@users.noreply.github.com>
2020-06-21 19:10:48 +0200

    feat(learn/mic): add button to release stream; and re-use stream if already acquired
    do not call `navigator.mediaDevices.getUserMedia` again
    
    but we do call `new MediaRecorder(stream);` again - not sure

--- src/app/apps/Learn/search-or-add-learnable-item/mic/mic.component.ts
+++ src/app/apps/Learn/search-or-add-learnable-item/mic/mic.component.ts
@@ -14,7 +14,8 @@ export class MicComponent implements OnInit {
 
   isRecording = false
   private mediaRecorder: any = null
-  private chunks: any[] = []
+  private audioChunks: any[] = []
+  stream: MediaStream
 
   constructor(
     public uploadService: UploadService,
@@ -25,55 +26,63 @@ export class MicComponent implements OnInit {
 
   onMicClick(event?) {
     if (event) {
-      event.preventDefault()
+      event.preventDefault() // prevent mouse click emulation from touchstart event because we have on-click as well
     }
     if ( this.isRecording ) {
       this.mediaRecorder.stop()
       this.isRecording = false
-      return;
+    } else {
+      this.startRecording()
     }
-    this.startRecording()
   }
 
   private startRecording() {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      console.log('getUserMedia supported.');
-
-      navigator.mediaDevices.getUserMedia(
-        {
-          audio: true, // constraints - only audio needed for this app
-        })
-        .then((stream) => {
-          this.isRecording = true
-          // Success callback
-          this.mediaRecorder = new MediaRecorder(stream);
-          this.mediaRecorder.start();
-          console.log(this.mediaRecorder.state);
-          console.log("recorder started");
-
-          this.chunks = [];
-
-          this.mediaRecorder.ondataavailable = (e) => {
-            this.chunks.push(e.data);
-          }
-          this.mediaRecorder.onstop = (e) => {
-            this.onRecordStopped()
-          }
-
-        })
-        .catch(function (err) {
-            window.alert('The following getUserMedia error occurred: ' + err);
-          },
-        );
+    if ( this.stream ) {
+      this.recordUsingStream(this.stream)
     } else {
-      window.alert('getUserMedia not supported on your browser!');
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+        // http://mdn.github.io/web-dictaphone/
+        // https://github.com/mdn/web-dictaphone/
+        console.log('getUserMedia supported.');
+        navigator.mediaDevices.getUserMedia(
+          {
+            audio: true, // constraints - only audio needed for this app
+          })
+          .then((stream) => {
+            this.stream = stream
+            this.recordUsingStream(stream)
+          })
+          .catch(function (err) {
+              window.alert('The following getUserMedia error occurred: ' + err);
+            },
+          );
+      } else {
+        window.alert('getUserMedia not supported on your browser!');
+      }
+    }
+  }
+
+  private recordUsingStream(stream: MediaStream) {
+    this.isRecording = true
+    this.mediaRecorder = new MediaRecorder(stream); // not sure if this is needed again after stopping previous
+    this.mediaRecorder.start();
+    console.log(`MediaRecorder started: this.mediaRecorder.state`, this.mediaRecorder.state);
+
+    this.audioChunks = [];
+
+    this.mediaRecorder.ondataavailable = (e) => {
+      this.audioChunks.push(e.data);
+    }
+    this.mediaRecorder.onstop = (e) => {
+      this.onRecordStopped()
     }
   }
 
   private onRecordStopped() {
-    console.log("recorder stopped");
-    const blob = new Blob(this.chunks, { 'type' : 'audio/ogg; codecs=opus' });
-    this.chunks = [];
+    console.log("stopping recording");
+    const blob = new Blob(this.audioChunks, { 'type' : 'audio/ogg; codecs=opus' });
+    this.audioChunks = [];
     const audioURL = window.URL.createObjectURL(blob);
     console.log(`audioURL`, audioURL)
     console.log(`learnDoService`, this.learnDoService)
@@ -82,11 +91,15 @@ export class MicComponent implements OnInit {
     learnItem.whenAdded = new Date()
     learnItem.saveNowToDb()
     this.uploadService.uploadAudio2(blob, learnItem.id)
-    // audio.src = audioURL;
+  }
 
-    // deleteButton.onclick = function(e) {
-    //   let evtTgt = e.target;
-    //   evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-    // }
+  releaseMic() {
+    // this.stream.stop() // https://developers.google.com/web/updates/2015/07/mediastream-deprecations?hl=en#stop-ended-and-active
+    // https://stackoverflow.com/questions/35977831/removing-the-recording-icon-mediastreamrecorder-js-library
+    // https://stackoverflow.com/questions/11642926/stop-close-webcam-which-is-opened-by-navigator-getusermedia
+    this.stream.getTracks().forEach(function(track) {
+      track.stop();
+    });
+    this.stream = undefined
   }
 }
