787110c7fadb7a9ed2dabec7c886fd25c4d0e601
Karol Depka <karol-depka@users.noreply.github.com>
2020-07-19 10:53:01 +0200

    @HostListener('window:beforeunload'  with sync service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import {TimerNotificationsService} from "./core/timer-notifications.service";
 import {AuthService} from "./core/auth.service";
+import {SyncStatusService} from './libs/AppFedShared/odm/sync-status.service'
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,7 @@ export class AppComponent {
     private statusBar: StatusBar,
     private timerNotificationService /* force the service to run */: TimerNotificationsService,
     private authService  /* force the service to run */: AuthService,
+    public syncStatusService: SyncStatusService,
   ) {
     this.initializeApp();
   }
@@ -27,6 +29,21 @@ export class AppComponent {
       this.statusBar.overlaysWebView(false)
 
       this.splashScreen.hide();
+      console.log('initializeApp ...')
     });
   }
+
+  @HostListener('window:beforeunload', ['$event'])
+  handleBeforeUnload($event: any) {
+    if ( this.syncStatusService.syncStatus$.lastVal ?. pendingUploadsCount ) {
+      // TODO: record throttled patches too
+      // https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event
+      // Cancel the event as stated by the standard.
+      $event.preventDefault();
+      // Chrome requires returnValue to be set.
+      $event.returnValue = 'Your data will be lost!';
+      /// TODO: https://developer.mozilla.org/en-US/docs/Web/API/Window/pagehide_event
+    }
+  }
+
 }
