332ea6c226071979bb245dbbb95d96c245a83a29
Karol Depka <karol-depka@users.noreply.github.com>
2020-07-25 17:11:19 +0200

    feat(learn): stats: "at least x rating" items count in service

--- src/app/apps/Learn/core/learn-stats.service.ts
+++ src/app/apps/Learn/core/learn-stats.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import {countBy} from 'lodash'
 import {LearnItem, LearnItem$} from '../models/LearnItem'
-import {countNotNullishBy} from '../../../libs/AppFedShared/utils/utils'
+import {countNotNullishBy, nullish} from '../../../libs/AppFedShared/utils/utils'
 import {Dictionary} from '../../../libs/AppFedShared/utils/dictionary-utils'
 import {LearnDoService} from './learn-do.service'
 import {map} from 'rxjs/operators'
 import {Observable} from 'rxjs/internal/Observable'
 
+/** split into part that goes into DB */
 export class LearnStats {
   countWithRatingPresent?: number = 0
+  atLeast05? = 0
+  atLeast1? = 0
+  atLeast1_5? = 0
+  atLeast2? = 0
   countWithRatingEqual?: Dictionary<number> = {}
   countWithRatingEqualOrHigher?: Dictionary<number> = {}
 }
@@ -27,6 +32,10 @@ export class LearnStatsService {
       return {
         countWithRatingEqual: countBy(items, (item: LearnItem) => item ?. lastSelfRating),
         countWithRatingPresent: countNotNullishBy(items, item => item ?. lastSelfRating),
+        atLeast05: items ?. filter((item: LearnItem | nullish) => (item ?. lastSelfRating ?? 0) >= 0.5) ?. length,
+        atLeast1: items ?. filter((item: LearnItem | nullish) => (item ?. lastSelfRating ?? 0) >= 1) ?. length,
+        atLeast1_5: items ?. filter((item: LearnItem | nullish) => (item ?. lastSelfRating ?? 0) >= 1.5) ?. length,
+        atLeast2: items ?. filter((item: LearnItem | nullish) => (item ?. lastSelfRating ?? 0) >= 2) ?. length,
       }
     })
   )
