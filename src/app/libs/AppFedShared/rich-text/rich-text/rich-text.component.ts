63d202981ecc471f08d58cf0c176735ffc15c3e5
Karol Depka <karol-depka@users.noreply.github.com>
2020-08-02 18:08:17 +0200

    chore: add RichTextComponent

---
+++ src/app/libs/AppFedShared/rich-text/rich-text/rich-text.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * http://ckeditor.github.io/editor-recommendations/about/
+ **/
+@Component({
+  selector: 'app-rich-text',
+  templateUrl: './rich-text.component.html',
+  styleUrls: ['./rich-text.component.sass'],
+})
+export class RichTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
