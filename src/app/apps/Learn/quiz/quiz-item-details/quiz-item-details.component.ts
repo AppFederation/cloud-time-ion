ccd284bd04a46b5f2a8b547c430a13c9eb559990
Karol Depka <karol-depka@users.noreply.github.com>
2020-08-11 02:07:59 +0200

    improve(learn/quiz): use improved & html editor components

--- src/app/apps/Learn/quiz/quiz-item-details/quiz-item-details.component.ts
+++ src/app/apps/Learn/quiz/quiz-item-details/quiz-item-details.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {debugLog} from '../../../../libs/AppFedShared/utils/log'
+import {LearnItem, LearnItem$} from '../../models/LearnItem'
+import {Observable} from 'rxjs/internal/Observable'
+import {NumericPickerVal} from '../../../../libs/AppFedSharedIonic/ratings/numeric-picker/numeric-picker.component'
 
 @Component({
   selector: 'app-quiz-item-details',
@@ -7,8 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizItemDetailsComponent implements OnInit {
 
-  constructor() { }
+  public shouldShowAnswer = false
+
+  selfRating: NumericPickerVal | undefined = undefined
+
+  @Input()
+  item$ ? : LearnItem$ | null
+
+  get itemVal$(): Observable<LearnItem | undefined | null> | undefined {
+    return this.item$ ?. locallyVisibleChanges$
+  }
+
+  constructor() {
+    debugLog('QuizItemDetailsComponent ctor')
+  }
 
   ngOnInit() {}
 
+  showAnswer() {
+    this.shouldShowAnswer = ! this.shouldShowAnswer
+  }
+
+  // onChangeSelfRating($event: NumericPickerVal) {
+  // }
+  applyAndNext() {
+    this.reset()
+
+    this.item$ ?. setNewSelfRating(this.selfRating !)
+  }
+
+  private reset() {
+    this.shouldShowAnswer = false
+  }
 }
--- src/app/apps/Learn/quiz/quiz.page.ts
+++ src/app/apps/Learn/quiz/quiz.page.ts
@@ -5,13 +5,13 @@ import {map, switchMap} from 'rxjs/operators'
 import {NumericPickerVal} from '../../../libs/AppFedSharedIonic/ratings/numeric-picker/numeric-picker.component'
 import {LearnItem, LearnItem$} from '../models/LearnItem'
 import {Observable} from 'rxjs/internal/Observable'
-import {nullish} from '../../../libs/AppFedShared/utils/utils'
 import {PopoverController} from '@ionic/angular'
 import {SyncPopoverComponent} from '../../../libs/AppFedShared/odm/sync-status/sync-popover/sync-popover.component'
 import {QuizTimerPopoverComponent} from './quiz-timer-popover/quiz-timer-popover.component'
 import {FormControl, FormGroup} from '@angular/forms'
 import {mapFieldsToFormControls} from '../../../libs/AppFedShared/utils/dictionary-utils'
 import {ViewSyncer} from '../../../libs/AppFedShared/odm/ui/ViewSyncer'
+import {nullish} from '../../../libs/AppFedShared/utils/type-utils'
 
 
 @Component({
@@ -20,11 +20,6 @@ import {ViewSyncer} from '../../../libs/AppFedShared/odm/ui/ViewSyncer'
   styleUrls: ['./quiz.page.sass'],
 })
 export class QuizPage implements OnInit {
-
-  public shouldShowAnswer = false
-  dummyArray = [{}]
-
-  selfRating: NumericPickerVal | undefined = undefined
 
   item$: LearnItem$ | undefined
 
@@ -48,29 +43,6 @@ export class QuizPage implements OnInit {
     )
   }
 
-  tinyMceInit = {
-    height: 500,
-    menubar: false,
-    plugins: [
-      'advlist autolink lists link image charmap print preview anchor',
-      'searchreplace visualblocks code fullscreen',
-      'insertdatetime media table paste code help wordcount'
-    ],
-    toolbar:
-      'undo redo | formatselect | bold italic backcolor | \
-      alignleft aligncenter alignright alignjustify | \
-      bullist numlist outdent indent | removeformat | help',
-    skin: 'oxide-dark',
-    content_css: 'dark',  // > **Note**: This feature is only available for TinyMCE 5.1 and later.,
-    entity_encoding: `raw` /* https://www.tiny.cloud/docs-3x/reference/configuration/Configuration3x@entity_encoding/ */,
-  }
-
-  formControls ? : { [ key: string] : FormControl /* TODO: mapped type */ }
-  formGroup ? : FormGroup
-
-  public viewSyncer ? : ViewSyncer
-
-
   constructor(
     public quizService: QuizService,
     public popoverController: PopoverController,
@@ -91,36 +63,36 @@ export class QuizPage implements OnInit {
   ngOnInit() {
   }
 
-  getQuestionValForQuiz$() {
-    // FIXME: ! keep in mind this could be called multiple times due to change detection !
-    return this.item$$.pipe(map(item$ => {
-      if ( this.item$ !== item$ ) {
-        console.log(`getSideValForQuiz$ new item$`)
-        setTimeout(() => {
-          this.reset()
-        })
-      }
-      this.item$ = item$ // could be race condition?
-      if ( item$ && ! this.viewSyncer ) {
-        this.formControls = mapFieldsToFormControls(sidesDefs)
-        this.formGroup = new FormGroup(this.formControls !)
-        this.viewSyncer = new ViewSyncer(this.formGroup /* FIXME: replace with item-side component, for patching individual fields */, item$)
-      }
-        //   this.item$ = item$
-        //   // FIXME: this should improve a lot when I emit entire array changed (newly arrived), instead of each onAdded
-        // })
-        // console.log(`getSideValForQuiz$ this.item$$.pipe`)
-        if ( ! item$ ) {
-          return '(Loading...)'
-        }
-        const item = item$.currentVal
-        if ( ! item ) {
-          return '(none)'
-        }
-        return item.getQuestionOrAnyString()
-      // }
-    }))
-  }
+  // getQuestionValForQuiz$() {
+  //   // FIXME: ! keep in mind this could be called multiple times due to change detection !
+  //   return this.item$$.pipe(map(item$ => {
+  //     if ( this.item$ !== item$ ) {
+  //       console.log(`getSideValForQuiz$ new item$`)
+  //       setTimeout(() => {
+  //         // this.reset()
+  //       })
+  //     }
+  //     this.item$ = item$ // could be race condition?
+  //     if ( item$ && ! this.viewSyncer ) {
+  //       this.formControls = mapFieldsToFormControls(sidesDefs)
+  //       this.formGroup = new FormGroup(this.formControls !)
+  //       this.viewSyncer = new ViewSyncer(this.formGroup /* FIXME: replace with item-side component, for patching individual fields */, item$)
+  //     }
+  //       //   this.item$ = item$
+  //       //   // FIXME: this should improve a lot when I emit entire array changed (newly arrived), instead of each onAdded
+  //       // })
+  //       // console.log(`getSideValForQuiz$ this.item$$.pipe`)
+  //       if ( ! item$ ) {
+  //         return '(Loading...)'
+  //       }
+  //       const item = item$.currentVal
+  //       if ( ! item ) {
+  //         return '(none)'
+  //       }
+  //       return item.getQuestionOrAnyString()
+  //     // }
+  //   }))
+  // }
 
   nowMs() {
     return Date.now()
@@ -130,20 +102,4 @@ export class QuizPage implements OnInit {
     return new Date(number)
   }
 
-  showAnswer() {
-    this.shouldShowAnswer = ! this.shouldShowAnswer
-  }
-
-  // onChangeSelfRating($event: NumericPickerVal) {
-  // }
-  applyAndNext() {
-    this.reset()
-
-    this.item$ ?. setNewSelfRating(this.selfRating !)
-  }
-
-  private reset() {
-    this.shouldShowAnswer = false
-    this.dummyArray = [{}]
-  }
 }
