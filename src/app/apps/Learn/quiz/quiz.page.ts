52bcc7ef113770ab5fadcac440a11947a79320db
Karol Depka <karol-depka@users.noreply.github.com>
2020-08-11 04:21:15 +0200

    Cleanup (old code) related to html editing

--- src/app/apps/Learn/quiz/quiz.page.ts
+++ src/app/apps/Learn/quiz/quiz.page.ts
@@ -1,17 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {QuizService} from '../core/quiz.service'
-import {sidesDefs, sidesDefsArray} from '../core/sidesDefs'
-import {map, switchMap} from 'rxjs/operators'
-import {NumericPickerVal} from '../../../libs/AppFedSharedIonic/ratings/numeric-picker/numeric-picker.component'
-import {LearnItem} from '../models/LearnItem'
 import {Observable} from 'rxjs/internal/Observable'
 import {PopoverController} from '@ionic/angular'
-import {SyncPopoverComponent} from '../../../libs/AppFedShared/odm/sync-status/sync-popover/sync-popover.component'
 import {QuizTimerPopoverComponent} from './quiz-timer-popover/quiz-timer-popover.component'
-import {FormControl, FormGroup} from '@angular/forms'
-import {mapFieldsToFormControls} from '../../../libs/AppFedShared/utils/dictionary-utils'
-import {ViewSyncer} from '../../../libs/AppFedShared/odm/ui/ViewSyncer'
-import {nullish} from '../../../libs/AppFedShared/utils/type-utils'
 import {LearnItem$} from '../models/LearnItem$'
 
 
@@ -23,14 +14,6 @@ import {LearnItem$} from '../models/LearnItem$'
 export class QuizPage implements OnInit {
 
   item$: LearnItem$ | undefined
-
-  get itemVal$(): Observable<LearnItem | nullish> {
-    return this.item$$.pipe(
-      switchMap(item$ => {
-        return item$ ?. locallyVisibleChanges$ !
-      })
-    )
-  }
 
   dePrioritizeNewMaterial: boolean = true
   onlyWithQA = false
@@ -63,37 +46,6 @@ export class QuizPage implements OnInit {
 
   ngOnInit() {
   }
-
-  // getQuestionValForQuiz$() {
-  //   // FIXME: ! keep in mind this could be called multiple times due to change detection !
-  //   return this.item$$.pipe(map(item$ => {
-  //     if ( this.item$ !== item$ ) {
-  //       console.log(`getSideValForQuiz$ new item$`)
-  //       setTimeout(() => {
-  //         // this.reset()
-  //       })
-  //     }
-  //     this.item$ = item$ // could be race condition?
-  //     if ( item$ && ! this.viewSyncer ) {
-  //       this.formControls = mapFieldsToFormControls(sidesDefs)
-  //       this.formGroup = new FormGroup(this.formControls !)
-  //       this.viewSyncer = new ViewSyncer(this.formGroup /* FIXME: replace with item-side component, for patching individual fields */, item$)
-  //     }
-  //       //   this.item$ = item$
-  //       //   // FIXME: this should improve a lot when I emit entire array changed (newly arrived), instead of each onAdded
-  //       // })
-  //       // console.log(`getSideValForQuiz$ this.item$$.pipe`)
-  //       if ( ! item$ ) {
-  //         return '(Loading...)'
-  //       }
-  //       const item = item$.currentVal
-  //       if ( ! item ) {
-  //         return '(none)'
-  //       }
-  //       return item.getQuestionOrAnyString()
-  //     // }
-  //   }))
-  // }
 
   nowMs() {
     return Date.now()
--- src/app/apps/Learn/shared/self-rating/self-rating.component.ts
+++ src/app/apps/Learn/shared/self-rating/self-rating.component.ts
@@ -31,22 +31,6 @@ export class SelfRatingComponent implements OnInit {
         this.item$.setNewSelfRating($event)
       }
     }
-
-    // const item = this.item$.currentVal
-    // const previousRating = item.lastSelfRating
-    // let newRating = $event
-    // if ( newRating === 2 && previousRating >= 2 ) {
-    //   const enoughTimePassed = false // TODO: based on calculation
-    //   if ( enoughTimePassed ) {
-    //     newRating = previousRating + 1
-    //   }
-    // }
-    //
-    // this.item$.patchThrottled({
-    //   lastSelfRating: newRating,
-    //   whenLastSelfRated: OdmBackend.nowTimestamp(),
-    //   selfRatingsCount: (item.selfRatingsCount || 0) + 1,
-    // })
   }
 
 }
