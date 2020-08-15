0d3bc642184b53e4076fafbdf56229afb0cc94c4
Karol Depka <karol-depka@users.noreply.github.com>
2020-08-15 03:32:05 +0200

    add types: DurationMs, TimeMsEpoch

--- src/app/libs/AppFedShared/utils/type-utils.ts
+++ src/app/libs/AppFedShared/utils/type-utils.ts
@@ -2,3 +2,7 @@
 export type nullish = null | undefined
 
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+
+export type DurationMs = number
+
+export type TimeMsEpoch = number
