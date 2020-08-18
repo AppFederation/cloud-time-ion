4367a36a92f3392eec7dbe6bae6b10c756b52fae
Karol Depka <karol-depka@users.noreply.github.com>
2020-08-18 03:56:44 +0200

    change from debounceTime to throttleTime

--- src/app/libs/AppFedShared/utils/rxUtils.ts
+++ src/app/libs/AppFedShared/utils/rxUtils.ts
@@ -4,21 +4,21 @@ import {Observable} from 'rxjs/internal/Observable'
 import {CachedSubject} from './cachedSubject2/CachedSubject2'
 
 export function throttleTimeWithLeadingTrailing<T>(timeMs: number) {
-  return debounceTime(timeMs)
+  // return debounceTime(timeMs)
   /* throtleTime appears to not work (fires immediately?)
     potentially related: https://github.com/ReactiveX/rxjs/issues/3712
    */
 
 
   // return throttleTime(timeMs)
-  // return throttleTime<T>(
-  //   timeMs,
-  //   async,
-  //   {
-  //     leading: true,
-  //     trailing: true,
-  //   }
-  // )
+  return throttleTime<T>(
+    timeMs,
+    async,
+    {
+      leading: true,
+      trailing: true,
+    }
+  )
 }
 
 export interface PatchableObservable<TInMemData, TMemPatch> {
