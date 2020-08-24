6fe0a7d2f15329e0cfce77d818a52b68ff2884de
Karol Depka <karol-depka@users.noreply.github.com>
2020-08-24 06:19:06 +0200

    add xAsMs util functions

--- src/app/libs/AppFedShared/utils/time-utils.ts
+++ src/app/libs/AppFedShared/utils/time-utils.ts
@@ -1,11 +1,19 @@
 import {DurationMs, TimeMsEpoch} from './type-utils'
 
-export function getEndOfDay() {
-  // return new
-}
+// export function getEndOfDay() {
+//   // return new
+// }
 
 export function hoursAsMs(hours: number): DurationMs {
-  return hours * 3600 * 1000;
+  return hours * 3600_000;
+}
+
+export function minutesAsMs(minutes: number): DurationMs {
+  return minutes * 60_000;
+}
+
+export function secondsAsMs(seconds: number): DurationMs {
+  return seconds * 1000;
 }
 
 export function isInFuture(timeMsEpoch: TimeMsEpoch) {
