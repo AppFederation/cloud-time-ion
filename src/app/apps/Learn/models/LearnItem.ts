f628555723940c55b23d3f9812018837dc3cad1c
Karol Depka <karol-depka@users.noreply.github.com>
2020-08-27 04:09:31 +0200

    add class FunDescriptors

--- src/app/apps/Learn/models/LearnItem.ts
+++ src/app/apps/Learn/models/LearnItem.ts
@@ -28,6 +28,18 @@ export class ImportanceDescriptors {
    */ // 2.5 / 3
 }
 
+export class FunDescriptors {
+  disgusting        = 0 // 0    BTN
+  very_low          = 1 // 0.5
+  low               = 2 // 1    BTN
+  medium            = 5 // 1.5 // default when unspecified
+  high              = 10 // 2   BTN
+  very_high/*fun*/   = 20 /* just 4 times more than unspecified?? --> 10 times?
+      20 times higher than very_low seems ok
+   */ // 2.5 / 3
+}
+
+
 /** LearnDoItemData */
 export class LearnItem extends OdmInMemItem {
   id?: LearnItemId
