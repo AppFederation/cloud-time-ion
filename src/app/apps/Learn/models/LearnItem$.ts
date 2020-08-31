4c5564fe0c059a242e66794480814fcf0b4bdc42
Karol Depka <karol-depka@users.noreply.github.com>
2020-08-31 11:46:23 +0200

    reformat LearnItem to 2 spaces

--- src/app/apps/Learn/models/LearnItem$.ts
+++ src/app/apps/Learn/models/LearnItem$.ts
@@ -5,29 +5,28 @@ import {OdmBackend} from '../../../libs/AppFedShared/odm/OdmBackend'
 import {LearnItem} from './LearnItem'
 
 export class LearnItem$
-  extends OdmItem$2<LearnItem$, LearnItem, LearnItem, LearnDoService>
-{
+  extends OdmItem$2<LearnItem$, LearnItem, LearnItem, LearnDoService> {
 
-    // TODO: operations should actually be performed on certain Version, for versioning, drafts, branches, conflict detection/resolution
+  // TODO: operations should actually be performed on certain Version, for versioning, drafts, branches, conflict detection/resolution
 
-    setNewSelfRating(newSelfRatingFromUser: NumericPickerVal) {
-        const item = this.currentVal
-        const previousRating = item?.lastSelfRating // ?? 0
-        let newEffectiveRating = newSelfRatingFromUser
-        // repeatedly self-rating as 2 or 2.5, increases effective rating (confidence) :
-        if (newEffectiveRating >= 2 /* in case of 2.5 (click 2 times) */ && (previousRating ?? 0) >= 2) {
-            const enoughTimePassed = true // TODO: based on calculation
-            if (enoughTimePassed) {
-              const increaseBy = (newSelfRatingFromUser < 2.5) ? 1 : 2
-              newEffectiveRating = previousRating ! + increaseBy
-            }
-        }
-
-        this.patchThrottled({
-            lastSelfRating: newEffectiveRating,
-            whenLastSelfRated: OdmBackend.nowTimestamp(),
-            selfRatingsCount: (item?.selfRatingsCount || 0) + 1,
-        })
-
+  setNewSelfRating(newSelfRatingFromUser: NumericPickerVal) {
+    const item = this.currentVal
+    const previousRating = item?.lastSelfRating // ?? 0
+    let newEffectiveRating = newSelfRatingFromUser
+    // repeatedly self-rating as 2 or 2.5, increases effective rating (confidence) :
+    if (newEffectiveRating >= 2 /* in case of 2.5 (click 2 times) */ && (previousRating ?? 0) >= 2) {
+      const enoughTimePassed = true // TODO: based on calculation
+      if (enoughTimePassed) {
+        const increaseBy = (newSelfRatingFromUser < 2.5) ? 1 : 2
+        newEffectiveRating = previousRating ! + increaseBy
+      }
     }
+
+    this.patchThrottled({
+      lastSelfRating: newEffectiveRating,
+      whenLastSelfRated: OdmBackend.nowTimestamp(),
+      selfRatingsCount: (item?.selfRatingsCount || 0) + 1,
+    })
+
+  }
 }
