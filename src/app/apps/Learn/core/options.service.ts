69067d6f9291e3586e277d7f082aa85cbf874b37
Karol Depka <karol-depka@users.noreply.github.com>
2020-09-04 19:51:10 +0200

    Add class LocalOptionsPatchableObservable<TOptions>

--- src/app/apps/Learn/core/options.service.ts
+++ src/app/apps/Learn/core/options.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import {DictPatch, PatchableObservable} from '../../../libs/AppFedShared/utils/rxUtils'
+import {CachedSubject} from '../../../libs/AppFedShared/utils/cachedSubject2/CachedSubject2'
+import {patch} from '../../../libs/AppFedShared/utils/dictionary-utils'
+import {debugLog} from '../../../libs/AppFedShared/utils/log'
+
+export class LocalOptionsPatchableObservable<TOptions> implements PatchableObservable<TOptions> {
+
+  get val() {
+    return this.locallyVisibleChanges$.lastVal
+  }
+
+  constructor(
+    public defaultValue: TOptions,
+    public localStorageKey = 'LifeSuite_options'
+  ) {
+    this.locallyVisibleChanges$.next(JSON.parse(localStorage.getItem(localStorageKey) || '{}') ?? defaultValue)
+  }
+
+
+  locallyVisibleChanges$: CachedSubject<any | undefined | null> = new CachedSubject<TOptions>({} as TOptions)
+
+  patchThrottled(patchToApply: DictPatch<any>): void {
+    this.locallyVisibleChanges$.next(patch(this.locallyVisibleChanges$.lastVal, patchToApply))
+    debugLog(`localStorage.setItem`, patchToApply)
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.val))
+  }
+
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class OptionsService {
+
+  // options$ = new LocalOptionsPatchableObservable<LifeSuiteOptions>(
+  //   new LifeSuiteOptions()
+  // )
 
   // use PatchableObservable<TInMemData, TMemPatch> for bindability with ViewSyncer
 
