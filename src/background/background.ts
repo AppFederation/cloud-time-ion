155e884562b1ce57753b600f2a03987f93091940
noviodelangel <mateuszwerpulewski@gmail.com>
2020-09-21 01:49:22 +0200

    Basic popup working

---
+++ src/background/background.ts
@@ -0,0 +1,30 @@
+import { environment } from '../environments/environment';
+
+// @ts-ignore
+firebase.initializeApp(environment.firebaseConfig);
+
+// auto login on startup
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+        if (request.command === 'login') {
+                // @ts-ignore
+                firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
+                    .then((userCredential) => {
+                            console.log(`Logged in: ${userCredential.user?.uid}`);
+                            sendResponse(userCredential);
+                    })
+                    .catch((error) => {
+                            console.log(`Sign in error: ${error}`);
+                            sendResponse(error);
+                    });
+        }
+});
+
+chrome.contextMenus.create({
+        title: 'Add Task',
+        id: 'addTask',
+        contexts: ['selection']
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+});
