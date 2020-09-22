87a8847848fe6c58769f73225321fdf4e9cbfb20
Karol Depka <karol-depka@users.noreply.github.com>
2020-09-22 15:59:17 +0200

    Add RichTextComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import {NgModule} from "@angular/core";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {IonicModule} from "@ionic/angular";
+import {RichTextComponent} from '../libs/AppFedShared/rich-text/rich-text/rich-text.component'
+import {EditorModule} from '@tinymce/tinymce-angular'
 
 let imports = [
   ReactiveFormsModule,
@@ -8,13 +10,20 @@ let imports = [
 ];
 
 @NgModule({
-  entryComponents: [],
+    entryComponents: [],
   imports: [
     IonicModule.forRoot(),
     ...imports,
+    EditorModule,
   ],
-  exports: imports,
-  providers: [],
+    exports: [
+        imports,
+        RichTextComponent,
+    ],
+    providers: [],
+    declarations: [
+        RichTextComponent,
+    ],
 })
 export class SharedModule {
 }
