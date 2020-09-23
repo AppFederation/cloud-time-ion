b8f56bb95cb1a2bebd61dd5db97ce7dcfd2f239e
Karol Depka <karol-depka@users.noreply.github.com>
2020-09-23 17:31:28 +0200

    improve(journal): add rich text editing

---
+++ src/app/apps/Journal/journal-write-page/journal-text-fields/journal-text-field/journal-text-field.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { JournalTextFieldComponent } from './journal-text-field.component';
+
+describe('JournalTextFieldComponent', () => {
+  let component: JournalTextFieldComponent;
+  let fixture: ComponentFixture<JournalTextFieldComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ JournalTextFieldComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(JournalTextFieldComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
