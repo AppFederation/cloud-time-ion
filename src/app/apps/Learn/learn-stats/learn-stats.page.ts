7a6095637ce99c1d4c2136b31b71abc092fdfc8f
Nekmo <contacto@nekmo.com>
2020-09-30 00:14:39 +0200

    Issue #45: Chart showing progress of learning (Include async data in stacked-interactive-chart for load data from api).

--- src/app/apps/Learn/learn-stats/learn-stats.page.ts
+++ src/app/apps/Learn/learn-stats/learn-stats.page.ts
@@ -1,51 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {StatsHistoryService} from '../core/stats-history.service'
 import {map} from 'rxjs/operators'
+import {HttpClient} from '@angular/common/http'
+// import {dataGenerator} from './learn-stats-demo-data';
 
-
-function getRandomInt(min: number, max: number) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-
-function* dataGenerator(days: number, start_at: number) {
-  let i = 0;
-  while (i < days) {
-    const seconds = start_at + (i * 3600 * 24);
-    yield {
-      "countByRating": {
-        "0": getRandomInt(0, 5),
-        "1": getRandomInt(0, 10),
-        "2": getRandomInt(0, 20),
-        "3": getRandomInt(0, 30),
-        "4": getRandomInt(0, 50),
-        "5": getRandomInt(0, 60),
-        "6": getRandomInt(0, 70),
-        "7": getRandomInt(0, 30),
-        "0.5": getRandomInt(0, 10),
-        "1.5": getRandomInt(0, 5),
-        "2.5": getRandomInt(0, 5),
-        "4.5": getRandomInt(0, 50),
-        "5.5": getRandomInt(0, 50),
-        "undefined": getRandomInt(100, 400)
-      },
-      "countWithAudio": 0,
-      "countWithQA": 262,
-      "owner": "7Tbg0SwakaVoCXHlu1rniHQ6gwz1",
-      "whenCreated": {
-        "seconds": seconds,
-        "nanoseconds": 0
-      },
-      "whenLastModified": {
-        "seconds": seconds,
-        "nanoseconds": 0
-      }
-    }
-    i++;
-  }
-}
+const API_STATS_URL = '/path/to/stats';  // TODO
 
 
 function* dataToSingleValues(dataset: any) {
@@ -64,7 +23,8 @@ function* dataToSingleValues(dataset: any) {
 const DEMO_CHART = {
   "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
   // "data": {"url": "assets/unemployment-across-industries.json"},
-  "data": {"values": Array.from(dataToSingleValues(dataGenerator(30, new Date().getTime() / 1000)))},
+  // "data": {"values": Array.from(dataToSingleValues(dataGenerator(30, new Date().getTime() / 1000)))},
+  "data": {"name": "data", "values": []},
   "vconcat": [{
     "width": 480,
     "mark": "area",
@@ -120,12 +80,20 @@ export class LearnStatsPage implements OnInit {
   ))
 
   spec: {} = DEMO_CHART;
+  data: {}[] = [];
 
   constructor(
     public statsHistoryService: StatsHistoryService,
+    public http: HttpClient,
   ) { }
 
   ngOnInit() {
+    // Use demo data:
+    // this.data = Array.from(dataToSingleValues(dataGenerator(30, new Date().getTime() / 1000)));
+
+    this.http.get(API_STATS_URL).subscribe((data) => {
+      this.data = Array.from(dataToSingleValues(data));
+    });
   }
 
 }
--- src/app/apps/Learn/shared/stacked-interactive-chart/stacked-interactive-chart.component.ts
+++ src/app/apps/Learn/shared/stacked-interactive-chart/stacked-interactive-chart.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 
-import embed from 'vega-embed';
+import embed, {Result} from 'vega-embed';
 
 
 @Component({
@@ -12,6 +12,8 @@ import embed from 'vega-embed';
 export class StackedInteractiveChartComponent implements OnInit, OnChanges {
 
   @Input() spec: {} | null = null;  // https://vega.github.io/schema/vega-lite/v4.json
+  @Input() data: {}[] = [];
+  res: Result | null = null;
 
   constructor() { }
 
@@ -23,12 +25,24 @@ export class StackedInteractiveChartComponent implements OnInit, OnChanges {
     if(changes.spec) {
       this.reloadChart();
     }
+    if(changes.data && this.res) {
+      this.setData();
+    }
   }
 
   public vegaLiteInit(spec: {} | null) {
     console.log('vega');
     // @ts-ignore
-    embed(`#chart`, spec).then();
+    embed(`#chart`, spec).then((res: Result) => {
+      this.res = res;
+      this.setData();
+    });
+  }
+
+  public setData() {
+    if (this.res) {
+      this.res.view.insert('data', this.data).run()
+    }
   }
 
   public reloadChart() {
