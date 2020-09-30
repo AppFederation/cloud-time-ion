e3e091360ad30a878674810ebd7d9dd5c2e91003
Karol Depka <karol-depka@users.noreply.github.com>
2020-09-30 04:36:08 +0200

    Add stub of DictionaryService for dictionary / definition API-s

---
+++ src/app/apps/Learn/core/dictionary.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+/* https://rapidapi.com/blog/dictionary-apis/
+*
+* https://dictionaryapi.com/
+*
+* https://developer.oxforddictionaries.com/ - 1000 requests per month
+*
+* https://yandex.com/dev/dictionary/ - https://yandex.com/dev/dictionary/keys/get/
+*
+*  */
+@Injectable({
+  providedIn: 'root'
+})
+export class DictionaryService {
+
+  constructor() { }
+}
