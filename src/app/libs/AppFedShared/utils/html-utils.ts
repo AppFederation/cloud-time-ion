e69a9c8c474fcd663fd0a82c93513f107590ecad
Karol Depka <karol-depka@users.noreply.github.com>
2020-10-01 08:44:45 +0200

    add convertToHtmlIfNeeded util func

--- src/app/libs/AppFedShared/utils/html-utils.ts
+++ src/app/libs/AppFedShared/utils/html-utils.ts
@@ -1,6 +1,24 @@
-export function stripHtml(html: string) {
+export function stripHtml(html?: string | null) {
+  if ( ! html ) {
+    return html
+  }
   const doc = new DOMParser().parseFromString(html, 'text/html');
   return doc.body.textContent || '';
 
   // TODO: to consider: href links for searching
 }
+
+export function convertToHtmlIfNeeded(htmlOrPlainString?: string | null) {
+  if ( ! htmlOrPlainString ) {
+    return htmlOrPlainString
+  }
+  if ( ! htmlOrPlainString?.match(/^\s*<\w+>.*/) ) {
+    // console.log('convertToHtmlIfNeeded is plaintext:', htmlOrPlainString)
+    htmlOrPlainString = '<p></p>' /* just to mark it as html */ +
+      htmlOrPlainString.replace(/\n/g, `<br>`)
+  } else {
+    // console.log('convertToHtmlIfNeeded is html:', htmlOrPlainString)
+  }
+  return htmlOrPlainString;
+}
+
