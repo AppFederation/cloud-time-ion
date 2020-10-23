db95baa051d82ab84e9f2d810272c98f358d1272
Karol Depka <karol-depka@users.noreply.github.com>
2020-10-23 17:01:44 +0200

    improve(journal): sort journal entries in reverse

--- src/app/apps/Journal/journal-entries-list/journal-entries-list.page.ts
+++ src/app/apps/Journal/journal-entries-list/journal-entries-list.page.ts
@@ -3,7 +3,7 @@ import {JournalEntriesService} from '../core/journal-entries.service'
 import {JournalEntry$} from '../models/JournalEntry$'
 import {CachedSubject} from '../../../libs/AppFedShared/utils/cachedSubject2/CachedSubject2'
 import {of} from 'rxjs'
-import {delay} from 'rxjs/operators'
+import {delay, map} from 'rxjs/operators'
 import {debugLog} from '../../../libs/AppFedShared/utils/log'
 import {JournalEntry} from '../models/JournalEntry'
 
@@ -23,6 +23,14 @@ export class JournalEntriesListPage implements OnInit {
 
   items$: CachedSubject<JournalEntry$[]> = this.journalEntriesService.localItems$
   // itemsHack ? : JournalEntry$[]
+  items$Sorted = this.items$.pipe(
+    map((item$s: JournalEntry$[]) => {
+      return item$s.sort((item$1, item$2) => {
+        return ((item$2.val?.whenCreated ?? (item$2.val as any)?.whenAdded)?.toDate().getTime() ?? 0) -
+          ((item$1.val?.whenCreated ?? (item$1.val as any)?.whenAdded)?.toDate().getTime() ?? 0)
+      })
+    })
+  )
 
   constructor(
     public journalEntriesService: JournalEntriesService,
