18f3835d5d4d2b5923cd8170db29dd653dd087c1
Karol Depka <karol-depka@users.noreply.github.com>
2020-10-23 17:39:41 +0200

    add importance model

--- src/app/apps/Learn/models/LearnItem.ts
+++ src/app/apps/Learn/models/LearnItem.ts
@@ -3,6 +3,7 @@ import {OdmInMemItem} from '../../../libs/AppFedShared/odm/OdmItem$2'
 import {OdmTimestamp} from '../../../libs/AppFedShared/odm/OdmBackend'
 import {Side, SidesDefs, sidesDefsArray, sidesDefsHintsArray, SideVal} from '../core/sidesDefs'
 import {DurationMs, nullish} from '../../../libs/AppFedShared/utils/type-utils'
+import {Dict, dictToArrayWithIds} from '../../../libs/AppFedShared/utils/dictionary-utils'
 
 export type LearnItemId = OdmItemId<LearnItem>
 export type Rating = number
@@ -17,16 +18,34 @@ export class SelfRatingDescriptors {
 }
 
 /* Note: importance, not priority; priority is calculated based on other factors like estimations, deadlines, free time, FUN, etc. */
-export class ImportanceDescriptors {
-  off       = 0 // 0    BTN
-  very_low  = 1 // 0.5
-  low       = 2 // 1    BTN
-  medium    = 5 // 1.5 // default when unspecified;  should medium have a BTN?
-  high      = 10 // 2   BTN
-  very_high = 20 /* just 4 times more than unspecified?? --> 10 times?
+export class ImportanceDescriptors implements Dict<any> {
+  /* unset -> null; for querying */
+  off       = /* impDesc(*/ {numeric: 0, abbrev: `Off`} // 0    BTN
+  extremely_low  = {numeric: 0.5, abbrev: `XLP`} // this is better than off, coz it might re-occur at some point if I have a lot of time for learning, so I don't forget about it.
+  very_low  = {numeric: 1, abbrev: `VLP`} // 0.5
+  low       = {numeric: 2, abbrev: `LoP`} // 1    BTN
+  // somewhat / a bit low; SLP
+  somewhat_low = {numeric: 4, abbrev: `SLP`}
+  /* default between low and medium ? somewhat low? */
+  medium    = /* impDesc(*/ {numeric: 5, abbrev: `MdP`, id: `medium` /* hack */} // 1.5 // default when unspecified;  { should medium have a BTN? --> yes, coz we wanna be able to say that something was already manually deliberately prioritized; vs not prioritized yet (not prioritized could be also shown by "Process" btn maybe; or at least uncategorised ones)
+  // somewhat / a bit high; darkened up-chevron; SHP
+  somewhat_high = {numeric: 7, abbrev: `SHP`}
+  high      = {numeric: 10, abbrev: `HiP`} // 2   BTN
+  very_high = {numeric: 20, abbrev: `VHP`} /* just 4 times more than unspecified?? --> 10 times?
+   /* just 4 times more than unspecified?? --> 10 times?
       20 times higher than very_low seems ok
    */ // 2.5 / 3
+  extremely_high = {numeric: 40, abbrev: `XHP`}
+  // it gives 10 level total now
+
+  // Icons: up arrow (chevron), double up arrow, etc., medium: wavy, or flat line, or {up&down (but smth visually simple might be better)
 }
+
+export const importanceDescriptors = new ImportanceDescriptors()
+
+export const importanceDescriptorsArray = dictToArrayWithIds(importanceDescriptors as Dict<any>)
+
+export const importanceDescriptorsArrayFromHighest = importanceDescriptorsArray.slice().reverse()
 
 export class FunDescriptors {
   disgusting        = 0 // 0    BTN
@@ -53,6 +72,14 @@ export class LearnItem extends OdmInMemItem {
 
 
   selfRatingsCount?: number
+
+  /** synonyms: worth
+   * storing both name and numeric value, in case it changes in the future
+   */
+  importance? : {
+    id: keyof ImportanceDescriptors,
+    numeric: number,
+  }
 
   // idea: quizAvgMs ?: DurationMs /* can be calculated via quizTotalMs / selfRatingsCount, but we store for querying purposes */
   // idea: quizTotalMs ?: DurationMs
