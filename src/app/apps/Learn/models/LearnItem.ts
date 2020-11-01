2f49d3ccf0238b73fa7b7aace51a26d475284d29
Karol Depka <karol-depka@users.noreply.github.com>
2020-11-01 05:01:28 +0100

    shorter intensity abbreviations

--- src/app/apps/Learn/models/LearnItem.ts
+++ src/app/apps/Learn/models/LearnItem.ts
@@ -27,28 +27,28 @@ export type ImportanceDescriptor = any
 export class ImportanceDescriptors implements Dict<any> {
   /* unset -> null; for querying; should have highest effective importance, to force to decide */
   off       = /* impDesc(*/ intensity({numeric: 0, abbrev: `Off`}) // 0    BTN
-  unknown = intensity({ numeric: 50 /* a bit higher than extremely_high, to force decision later; but lower than effective numerical for unset */, abbrev: `Unk` /* / `?` */ })
+  unknown = intensity({ numeric: 50 /* a bit higher than extremely_high, to force decision later; but lower than effective numerical for unset */, abbrev: `?` /* / `?` */ })
   /* TODO: unknown / undecided = {} // different than off, medium or unset - meaning I went through it already (so it's not unset); #Workflow
    and I could not decide; e.g. I was lacking information at this point or did not have enough info. Example: watching some video which might, or might not be important. Need to first skim through it (which is a detour from going through a lot of items quickly)
   * one MIGHT go back to it and try to set it to a value later; which would probably be high-priority thing to do (prioritize prioritizing (via importance level))
     kinda similar to `null` in JS (vs `undefined`), but won't store undefined / null in Firestore
    */
 
-  extremely_low  = intensity({numeric: 0.5, abbrev: `XLP`}) // this is better than off, coz it might re-occur at some point if I have a lot of time for learning, so I don't forget about it.
-  very_low  = intensity({numeric: 1, abbrev: `VLP`}) // 0.5
-  low       = intensity({numeric: 2, abbrev: `LoP`}) // 1    BTN
+  extremely_low  = intensity({numeric: 0.5, abbrev: `XL`}) // this is better than off, coz it might re-occur at some point if I have a lot of time for learning, so I don't forget about it.
+  very_low  = intensity({numeric: 1, abbrev: `VL`}) // 0.5
+  low       = intensity({numeric: 2, abbrev: `Lo`}) // 1    BTN
   // somewhat / a bit low; SLP
-  somewhat_low = intensity({numeric: 4, abbrev: `SLP`})
+  somewhat_low = intensity({numeric: 4, abbrev: `SL`})
   /* default between low and medium ? somewhat low? */
-  medium    = /* impDesc(*/ intensity({numeric: 5, abbrev: `MdP`, id: `medium` /* hack */}) // 1.5 // default when unspecified;  { should medium have a BTN? --> yes, coz we wanna be able to say that something was already manually deliberately prioritized; vs not prioritized yet (not prioritized could be also shown by "Process" btn maybe; or at least uncategorised ones)
+  medium    = intensity({numeric: 5, abbrev: `Md`, id: `medium` /* hack */}) // 1.5 // default when unspecified;  { should medium have a BTN? --> yes, coz we wanna be able to say that something was already manually deliberately prioritized; vs not prioritized yet (not prioritized could be also shown by "Process" btn maybe; or at least uncategorised ones)
   // somewhat / a bit high; darkened up-chevron; SHP
-  somewhat_high = intensity({numeric: 7, abbrev: `SHP`})
-  high      = intensity({numeric: 10, abbrev: `HiP`}) // 2   BTN
-  very_high = intensity({numeric: 20, abbrev: `VHP`}) /* just 4 times more than unspecified?? --> 10 times?
+  somewhat_high = intensity({numeric: 7, abbrev: `SH`})
+  high      = intensity({numeric: 10, abbrev: `Hi`}) // 2   BTN
+  very_high = intensity({numeric: 20, abbrev: `VH`}) /* just 4 times more than unspecified?? --> 10 times?
    /* just 4 times more than unspecified?? --> 10 times?
       20 times higher than very_low seems ok
    */ // 2.5 / 3
-  extremely_high = intensity({numeric: 40, abbrev: `XHP`})
+  extremely_high = intensity({numeric: 40, abbrev: `XH`})
   // it gives 10 level total now
 
   // Icons: up arrow (chevron), double up arrow, etc., medium: wavy, or flat line, or {up&down (but smth visually simple might be better)
