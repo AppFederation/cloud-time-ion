54d918f569611abca7195e1c798a91ff58497e1b
Karol Depka <karol-depka@users.noreply.github.com>
2020-11-02 05:04:44 +0100

    Add CustomFormControl util class

---
+++ src/app/libs/AppFedShared/utils/angular/custom-form-control.ts
@@ -0,0 +1,35 @@
+import {ControlValueAccessor} from '@angular/forms'
+
+export abstract class CustomFormControl<TVal> implements ControlValueAccessor {
+
+  onChange ! : (newVal: TVal) => any;
+
+  private isDisabled = false
+
+  private value ? : TVal
+
+  constructor() {}
+
+  writeValue( value : TVal ) : void {
+    this.value = value
+  }
+
+  registerOnChange( fn : (newVal: TVal) => any ) : void {
+    this.onChange = fn;
+  }
+
+  fireOnChange($event: TVal ) {
+    this.onChange($event);
+  }
+
+  setDisabledState( isDisabled : boolean ) : void {
+    this.isDisabled = isDisabled
+  }
+
+  // writeValue(value: any): void { }
+  // registerOnChange(fn: any): void { }
+  /* TODO */
+  registerOnTouched(fn: any): void { }
+  // setDisabledState(isDisabled: boolean): void { }
+
+}
