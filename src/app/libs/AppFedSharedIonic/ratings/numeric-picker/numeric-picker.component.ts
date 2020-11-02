6890fa49b17ba21a97fe0d8df1199302d9364582
Karol Depka <karol-depka@users.noreply.github.com>
2020-11-02 05:07:02 +0100

    Use CustomFormControl in NumericPickerComponent

--- src/app/libs/AppFedSharedIonic/ratings/numeric-picker/numeric-picker.component.ts
+++ src/app/libs/AppFedSharedIonic/ratings/numeric-picker/numeric-picker.component.ts
@@ -1,8 +1,9 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {FormControl} from "@angular/forms";
+import {ChangeDetectionStrategy, Component, EventEmitter, forwardRef, Input, OnInit, Output} from '@angular/core';
+import {ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR} from "@angular/forms";
 import {JournalNumericDescriptor} from '../../../../apps/Journal/models/JournalNumericDescriptors'
 import {range} from 'lodash-es'
 import {debugLog} from '../../../AppFedShared/utils/log'
+import {CustomFormControl} from '../../../AppFedShared/utils/angular/custom-form-control'
 
 export class ButtonVariantDescriptor<TVal = any, TLabel = string | number> {
   constructor(
@@ -23,6 +24,21 @@ export class ButtonsDescriptor<TVal = any, TLabel = string | number> {
   constructor(
     public buttons: ButtonDescriptor<TVal, TLabel> []
   ) {}
+
+  /** Could return tuple of button and variant */
+  findVariantByValue(value: TVal | undefined): [ButtonDescriptor, ButtonVariantDescriptor] | undefined {
+    if ( ! value ) {
+      return undefined
+    }
+    for ( let button of this.buttons ) {
+      for ( let variant of button.btnVariants ) {
+        if ( (value as any).id === variant.id || value === variant.value ) {
+          return [button, variant] as any
+        }
+      }
+    }
+    return undefined
+  }
 }
 
 export function btn(x: ButtonDescriptor) {
@@ -62,8 +78,15 @@ function numAndFracBtn(x: number): ButtonDescriptor {
   templateUrl: './numeric-picker.component.html',
   styleUrls: ['./mood-picker.component.sass'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => NumericPickerComponent),
+      multi: true
+    }
+  ]
 })
-export class NumericPickerComponent implements OnInit {
+export class NumericPickerComponent<TVal = any> extends CustomFormControl<TVal> implements OnInit {
 
   @Output() numericValue = new EventEmitter<NumericPickerVal>()
 
@@ -78,20 +101,31 @@ export class NumericPickerComponent implements OnInit {
   @Input()
   buttonDescriptors ! : ButtonsDescriptor<any, any>
 
-  numValFormControl = new FormControl()
+  // numValFormControl = new FormControl()
 
   // numVal ? : NumericPickerVal
 
   selectedVariant ? : ButtonVariantDescriptor<any>
   selectedButton ? : ButtonDescriptor<any>
 
-  constructor() { }
+  constructor() {
+    super()
+  }
 
   ngOnInit() {
     if ( this.allowedVals && ! this.buttonDescriptors ) {
       this.buttonDescriptors = new ButtonsDescriptor<any, any>(
         this.allowedVals.map(numAndFracBtn)
       )
+    }
+  }
+
+  writeValue(value: TVal): void {
+    super.writeValue(value);
+    const found = this.buttonDescriptors.findVariantByValue(value)
+    if ( found ) {
+      this.selectedButton = found [ 0 ]
+      this.selectedVariant = found [ 1 ]
     }
   }
 
@@ -104,7 +138,7 @@ export class NumericPickerComponent implements OnInit {
     if ( previousSelectedButton === this.selectedButton ) {
       const currentVariantIdx = this.selectedButton.btnVariants.findIndex(
         (elem: ButtonVariantDescriptor) => elem.id === this.selectedVariant?.id)
-      debugLog(`currentVariantIdx`, currentVariantIdx, this.selectedButton.btnVariants, this.selectedVariant)
+      // debugLog(`currentVariantIdx`, currentVariantIdx, this.selectedButton.btnVariants, this.selectedVariant)
       newVariantIdx = (currentVariantIdx + 1) % btnVariants.length
     } else {
       newVariantIdx = 0
@@ -112,8 +146,10 @@ export class NumericPickerComponent implements OnInit {
     // debugLog(`newVariantIdx`, newVariantIdx)
     this.selectedVariant = btnVariants[newVariantIdx]
     const newValue = this.selectedVariant ?. value
-    debugLog(`value to emit`, newValue)
+    // debugLog(`value to emit`, newValue)
     this.numericValue.next(newValue)
+
+    this.fireOnChange(newValue)
     // // note this triggers change detection
   }
 
@@ -124,4 +160,5 @@ export class NumericPickerComponent implements OnInit {
   getDisplayedVariantForButton(btnDesc: ButtonDescriptor<any, any>): ButtonVariantDescriptor {
     return (this.isSelected(btnDesc) ? this.selectedVariant : btnDesc.btnVariants[0]) !
   }
+
 }
