691f8cfb953204201f734e1cc91b1933bc2eccb8
Karol Depka <karol-depka@users.noreply.github.com>
2020-11-13 06:22:52 +0100

    trying rsync in repo backup script

--- repoBackup.sh
+++ repoBackup.sh
@@ -4,8 +4,16 @@ shopt -s extglob
 
 dst=/S/Backups/test-repo-backup
 
-cp -r ./!(node_modules) $dst
+# cp -r ./!(node_modules|electron) $dst
 
 echo dot star
 
 # cp -r ./.* $dst
+
+rsync -av --progress . $dst  \
+    --exclude '**/node_modules'  \
+    --exclude platforms
+
+#    --exclude node_modules  \
+#    --exclude electron/node_modules  \
+
