f7c6f0607580c2bffc1f82dae6d53a904d2d147b
Karol Depka <karol-depka@users.noreply.github.com>
2020-11-15 18:05:20 +0100

    Add func incrementByPath() + spec

---
+++ src/app/libs/AppFedShared/utils/lodashPlus/incrementByPath.ts
@@ -0,0 +1,25 @@
+declare global {
+  interface Array<T> {
+    last() : T;
+  }
+}
+
+Array.prototype.last ??= function (this : string) {
+  return this[this.length - 1]
+};
+
+export function incrementByPath(obj: any, path: string[]): any {
+  let subObj = obj
+  for ( let i = 0; i < path.length - 1; ++i ) {
+    let curKey = path[i]
+    let newSubObj = subObj[curKey]
+    if ( ! newSubObj ) {
+      subObj[curKey] = {}
+    }
+    subObj = newSubObj
+  }
+  const lastKey = path.last()
+  const numVal = subObj[lastKey] ?? 0
+  subObj[lastKey] = numVal + 1
+  return obj
+}
