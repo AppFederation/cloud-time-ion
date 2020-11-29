626703e79227104a3c1ba129980b3d5bc6d2f4c6
Karol Depka <karol-depka@users.noreply.github.com>
2020-11-29 11:12:15 +0100

    Extract setOwnerIfNeeded to separate migration file

--- src/app/libs/AppFedSharedFirebase/odm-firestore/FirestoreOdmCollectionBackend.ts
+++ src/app/libs/AppFedSharedFirebase/odm-firestore/FirestoreOdmCollectionBackend.ts
@@ -59,26 +59,6 @@ export class FirestoreOdmCollectionBackend<TRaw> extends OdmCollectionBackend<TR
     })
   }
 
-  private setOwnerIfNeeded(docData: firebase.firestore.DocumentData, docId: string) {
-    if (this.collectionName === `JournalEntry`) {
-      const existingOwner = docData?.owner
-      const karolOwner = `7Tbg0SwakaVoCXHlu1rniHQ6gwz1`
-      if (!existingOwner) {
-        debugLog(this.collectionName + ` no owner `, existingOwner, docId)
-        this.itemDoc(docId).update({
-          owner: karolOwner
-        }).then(() => {
-          debugLog(`finished updating owner`, docId)
-        })
-      } else {
-        // debugLog(this.collectionName + ` yes owner `, docId, existingOwner)
-        if (existingOwner !== karolOwner) {
-          errorAlert(`some other owner!!`, `docId:`, docId, `owner: `, existingOwner)
-        }
-      }
-    }
-  }
-
   deleteWithoutConfirmation(itemId: OdmItemId) {
     ignorePromise(this.itemDoc(itemId).delete())
   }
