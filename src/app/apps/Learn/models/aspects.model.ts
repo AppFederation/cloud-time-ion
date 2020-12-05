fcf3202359307e0e9f0f8c22c5ddf0a2f9332b33
Karol Depka <karol-depka@users.noreply.github.com>
2020-12-05 18:12:08 +0100

    Add aspects model stub

---
+++ src/app/apps/Learn/models/aspects.model.ts
@@ -0,0 +1,20 @@
+
+/* aspects / concerns are also related to :
+* - sub-tasks
+* - task templates
+* - checklists (e.g. travel checklist)
+* - fields
+* TODO: move SidesDefs near here
+* */
+export class AspectsModel {
+  recon = {}
+  brainstorm = {}
+  design = {}
+  review = {}
+  testing = {}
+  examples = {}
+  benefits = {}
+  security = {}
+  ux = {}
+  requirements = {}
+}
