58c61bdfa4c2ee508512ec7ffa290a166f4f6789
Karol Depka <karol-depka@users.noreply.github.com>
2020-12-06 02:28:47 +0100

    Fix shared module imports/exports

--- src/app/libs/LifeSuiteShared/edit-shared/edit-shared.module.ts
+++ src/app/libs/LifeSuiteShared/edit-shared/edit-shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ImportanceEditComponent} from './importance-edit/importance-edit.component'
 import {SharedModule} from '../../../shared/shared.module'
+import {RatingsModule} from '../../AppFedSharedIonic/ratings/ratings.module'
 
 const exports = [
   ImportanceEditComponent
@@ -14,10 +15,12 @@ const exports = [
   exports: [
     ...exports,
     SharedModule,
+    RatingsModule,
   ],
   imports: [
     CommonModule,
     SharedModule,
+    RatingsModule,
   ]
 })
 export class EditSharedModule { }
