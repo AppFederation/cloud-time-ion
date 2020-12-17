54ae56af776d36e7fccadc31f96ce110767884c0
noviodelangel <mateuszwerpulewski@gmail.com>
2020-12-17 23:03:17 +0100

    Improving random content generator

--- src/app/generators/data-generator.service.ts
+++ src/app/generators/data-generator.service.ts
@@ -15,11 +15,25 @@ export class DataGeneratorService {
     let learnItemList: LearnItem$[] = [];
     for (let i = 0; i < count; i++) {
       let learnItem = new LearnItem();
-      learnItem.title = `Task ${i + 1}`;
+      learnItem.title = this.generateLines(50);
       let learnItem$ = new LearnItem$(this.learnDoService, i.toString(), learnItem);
 
       learnItemList.push(learnItem$);
     }
     return learnItemList;
+  }
+
+  private generateLines(maxLineCount: number): string {
+    let result = "";
+
+    for (let i = 0; i < Math.ceil(Math.random() * maxLineCount); i++) {
+      result = result.concat(this.generateRandomStringWithNewLineAtTheEnd());
+    }
+
+    return result;
+  }
+
+  private generateRandomStringWithNewLineAtTheEnd() {
+    return Math.random().toString(36).substring(2).concat("<br>");
   }
 }
