09ccdcb9decf71942eacee76a75437bbc852d89e
Karol Depka <karol-depka@users.noreply.github.com>
2020-12-26 04:34:15 +0100

    AutoBackup Sat Dec 26 04:34:15 CET 2020

---
+++ src/app/apps/Journal/journal-write-page/journal-item-edit/journal-item-edit.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-journal-item-edit',
+  templateUrl: './journal-item-edit.component.html',
+  styleUrls: ['./journal-item-edit.component.sass'],
+})
+export class JournalItemEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
