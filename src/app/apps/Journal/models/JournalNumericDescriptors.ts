c34beba9abad7085c3c746754bcf17502b5e6902
Karol Depka <karol-depka@users.noreply.github.com>
2021-01-16 17:48:47 +0100

    add more journal num fields

--- src/app/apps/Journal/models/JournalNumericDescriptors.ts
+++ src/app/apps/Journal/models/JournalNumericDescriptors.ts
@@ -428,6 +428,7 @@ export class JournalNumericDescriptors extends UiFieldDefs {
   concern = jnd({moderateIsBetter: true}) /* FIXME: de-duplicate; and with peace-of-mind */
   anxiety = jnd({lowerIsBetter: true}) /* FIXME: de-duplicate; and with peace-of-mind */
   tension = jnd({lowerIsBetter: true}) /* FIXME: de-duplicate; and with peace-of-mind; related to excessive excitement */
+  relief = jnd() /* FIXME: de-duplicate; and with peace-of-mind; related to excessive excitement */
   visualizing = jnd({moderateIsBetter: true})
   long_term_vision = jnd({moderateIsBetter: true, searchTerms: `long-term vision`})
   fun = jnd()
