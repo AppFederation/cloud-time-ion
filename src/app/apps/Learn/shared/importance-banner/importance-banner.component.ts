d67a52251d671bf57aa5bd612fe45957c4af8546
Karol Depka <karol-depka@users.noreply.github.com>
2021-02-14 04:47:45 +0100

    Importance banner distinguish type of importance

--- src/app/apps/Learn/shared/importance-banner/importance-banner.component.ts
+++ src/app/apps/Learn/shared/importance-banner/importance-banner.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {LearnItem$} from '../../models/LearnItem$'
 
 @Component({
   selector: 'app-importance-banner',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./importance-banner.component.sass'],
 })
 export class ImportanceBannerComponent implements OnInit {
+
+  @Input()
+  item$ ! : LearnItem$
 
   constructor() { }
 
