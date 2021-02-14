1947d391a4948594d6818978231420a59a206a3e
Karol Depka <karol-depka@users.noreply.github.com>
2021-02-14 04:17:00 +0100

    WIP ImportanceBannerComponent

---
+++ src/app/apps/Learn/shared/importance-banner/importance-banner.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-importance-banner',
+  templateUrl: './importance-banner.component.html',
+  styleUrls: ['./importance-banner.component.sass'],
+})
+export class ImportanceBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
