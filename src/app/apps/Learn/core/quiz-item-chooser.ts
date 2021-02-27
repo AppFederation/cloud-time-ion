16dfa984c56093c9f371e6a261368b9c66e07033
Karol Depka <karol-depka@users.noreply.github.com>
2021-02-27 18:04:39 +0100

    AutoBackup Sat Feb 27 18:04:39 CET 2021

--- src/app/apps/Learn/core/quiz-item-chooser.ts
+++ src/app/apps/Learn/core/quiz-item-chooser.ts
@@ -10,15 +10,16 @@ import {QuizOptions} from './quiz.service'
 export class QuizItemChooser {
 
   chooseItemFromPending(pendingItems: LearnItem$[], quizOptions: QuizOptions) {
-    // randomly pick importance
-
+    this.pickRandomByImportance()
 
     return this.findPendingItemOfHighestImportance(pendingItems, quizOptions)
   }
 
   private findPendingItemOfHighestImportance(pendingItems: LearnItem$[], quizOptions: QuizOptions): LearnItem$ | undefined {
+    // write separate func for random choice, and leave this one for now
     let nextItem$: LearnItem$ | undefined = undefined
     for (let importance of importanceDescriptorsArrayFromHighestNumeric) {
+      // FIXME SPEEDUP: don't go nImportanceDescriptors * nItems
       const filteredByImportance = pendingItems.filter(item$ => (item$.getEffectiveImportanceId()) === importance.id)
       // TODO: performance: could reuse itemsLeftByImportance from QuizStatus, instead of filtering (oh, but those are just counts; but could replace by groupBy; OTOH, we don't need all of the arrays, just the non-empty one with highest importance)
       if (filteredByImportance.length) {
@@ -33,5 +34,14 @@ export class QuizItemChooser {
 
   calculateWhenNextRepetitionMsEpoch(item$: LearnItem$, quizOptions: QuizOptions): TimeMsEpoch {
     return item$?.quiz?.calculateWhenNextRepetitionMsEpoch(quizOptions)
+  }
+
+  private pickRandomByImportance() {
+    // build map of importance-to-oldest-pending-item --
+    // build table of random-range-to-importance, checking which importances have a pending item
+
+    // randomly pick importance
+    Math.random()
+
   }
 }
