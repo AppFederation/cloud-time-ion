faa5994a1ae418b2b0bcacc729759594f4499d72
Karol Depka <karol-depka@users.noreply.github.com>
2021-03-06 16:43:30 +0100

    Notes & quotes about firestore queries restrictions with >=, array-contains (for perms, time, parent)

--- src/app/libs/AppFedSharedFirebase/odm-firestore/firestore-odm-backend.service.ts
+++ src/app/libs/AppFedSharedFirebase/odm-firestore/firestore-odm-backend.service.ts
@@ -5,6 +5,14 @@ import {debugLog, errorAlert} from "../../AppFedShared/utils/log";
 import {AngularFirestore} from "@angular/fire/firestore";
 import {OdmItem} from "../../AppFedShared/odm/OdmItem";
 
+/**
+ *
+ * Default/common queries params
+ *  - read/owner (permission) - mandatory! coz otherwise permission error!
+ *  - ...... You can use at most one **array-contains** clause per query. You can't combine array-contains with array-contains-any.
+ *  - part-of parent element (most probably not array-contains, coz would collide with read permissions array-contains
+ *  - >= timestamp - newer than (last-modified) - You can perform range (<, <=, >, >=) or not equals (!=) comparisons only on a single field, and you can include at most one array-contains or array-contains-any clause in a compound query:
+ * */
 @Injectable()
 export class FirestoreOdmBackend extends OdmBackend {
 
