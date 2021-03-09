11968dd6da753efd8ec0982200c477b3d04eb56b
Karol Depka <karol-depka@users.noreply.github.com>
2021-03-09 02:26:13 +0100

    Add methods to PivotTable

--- src/app/libs/AppFedShared/utils/pivot/pivot-table.ts
+++ src/app/libs/AppFedShared/utils/pivot/pivot-table.ts
@@ -6,11 +6,23 @@ export class PivotTable<TItem, TVal, TDim1, TDim2, TAggregate = TVal>
     public dim1Getter,
     public dim2Getter,
     public aggregateFunc,
+    // optionally could specify included/excluded dim vals
   ) {
   }
 
   public getAggregateVal(dim1: TDim1, dim2: TDim2) // : TAggregate
   {
     // we have the values stored for fast getting (e.g. for Angular)
+  }
+
+  /** for ngFor */
+  public getDim1Vals() // : TDim1[]
+  {
+
+  }
+
+  public getDim2Vals() // : TDim2[]
+  {
+
   }
 }
