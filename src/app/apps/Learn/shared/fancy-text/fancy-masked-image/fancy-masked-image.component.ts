c45a0ac64ee659212a7917451d8d655585b3d239
Karol Depka <karol-depka@users.noreply.github.com>
2021-03-14 01:20:40 +0100

    AutoBackup Sun Mar 14 01:20:40 CET 2021

---
+++ src/app/apps/Learn/shared/fancy-text/fancy-masked-image/fancy-masked-image.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fancy-masked-image',
+  templateUrl: './fancy-masked-image.component.html',
+  styleUrls: ['./fancy-masked-image.component.sass'],
+})
+export class FancyMaskedImageComponent implements OnInit {
+
+  // https://css-tricks.com/how-to-do-knockout-text/
+  // https://css-tricks.com/almanac/properties/m/mask-image/
+  // https://stackoverflow.com/questions/11924800/use-text-as-a-mask-on-background-image
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
