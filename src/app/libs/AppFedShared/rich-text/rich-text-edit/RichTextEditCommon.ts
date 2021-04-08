e2dbe0c2811013fb548f36fd03153d7614a037f5
Karol Depka <karol-depka@users.noreply.github.com>
2021-04-08 22:45:12 +0200

    Add RichTextEditCommon

---
+++ src/app/libs/AppFedShared/rich-text/rich-text-edit/RichTextEditCommon.ts
@@ -0,0 +1,7 @@
+
+export class RichTextEditCommon {
+  valid_classes = `fancy warning negative positive concept` /* TODO: also consider valid_elements / extended_valid_elements  - https://www.tiny.cloud/docs/configure/content-filtering/#exampleusingvalid_elements */
+
+}
+
+export const richTextEditCommon = new RichTextEditCommon()
