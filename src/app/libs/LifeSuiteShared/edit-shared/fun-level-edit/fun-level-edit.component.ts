2b9ffd1a42e125cf46caad5789f8699ffc12fd1e
Karol Depka <karol-depka@users.noreply.github.com>
2021-05-28 19:02:07 +0200

    minFunLevel quiz option progress after formGroup.valueChanges not firing

--- src/app/libs/LifeSuiteShared/edit-shared/fun-level-edit/fun-level-edit.component.ts
+++ src/app/libs/LifeSuiteShared/edit-shared/fun-level-edit/fun-level-edit.component.ts
@@ -9,7 +9,7 @@ import {funLevels} from '../../../../apps/Learn/models/fields/fun-level.model'
 
 const levels = funLevels
 
-const buttonsDesc = new ButtonsDescriptor<any, string>([
+export const buttonsDesc = new ButtonsDescriptor<any, string>([
   btn({
     btnVariants: [
       intensityBtnVariant(`😡` /*😒*/, levels.somewhat_low),
@@ -47,6 +47,8 @@ const buttonsDesc = new ButtonsDescriptor<any, string>([
 })
 export class FunLevelEditComponent implements OnInit {
 
+  @Input() formControl1 ! : FormControl
+
   buttonsDesc = buttonsDesc
 
   formGroup ! : FormGroup
@@ -65,6 +67,11 @@ export class FunLevelEditComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    if ( this.formControl1 ) {
+      // can use logical assignment operator to overwrite &&=  /  ||=  /  ??=
+      // https://www.typescriptlang.org/play#code/C4TwDgpgBAQg9nANlAvFARgxECGA7KAHyjwFdFljS8ATCAMwEs8IaBuAWAChvtgoAHgC5YWVFGAAnUhE48uAqADIlaejkQBnWVAD0u1AD4o6rRG6KVaMhTZ6DKYzcQXlqqNTpMW7e0Y+0DMys3LwQ-CAi8Ejiptpy3CBuaFIydvr+ceZcSVYk5IjpDsZZickBXsG+GY4mGtrcQA
+      this.formControls.funEstimate = this.formControl1
+    }
     this.formGroup = new FormGroup(this.formControls)
     if ( this . item$ ) {
       this.viewSyncer = new ViewSyncer(
