f7753bb61e578f653a9d412e61f1b8e0d63665a9
Karol Depka <karol-depka@users.noreply.github.com>
2021-12-17 02:03:06 +0100

    Change Mark tinymce button label to "M"

--- src/app/libs/AppFedShared/rich-text/rich-text-edit/rich-text-edit.component.ts
+++ src/app/libs/AppFedShared/rich-text/rich-text-edit/rich-text-edit.component.ts
@@ -179,7 +179,7 @@ export class RichTextEditComponent implements OnInit {
         });
       editor.ui.registry.addButton('customMarkBtn', {
         /* https://www.tiny.cloud/docs/demo/custom-toolbar-button/ */
-        text: 'MARK',
+        text: 'M',
         onAction: () => {
           this.highlightSelected(editor)
           // editor.insertContent('&nbsp;<strong>It\'s my button!</strong>&nbsp;');
