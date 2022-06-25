89ba3591de7174a8792074d47d30d95ffbbc634e
Karol Depka <karol-depka@users.noreply.github.com>
2022-06-25 05:09:28 +0200

    Show/hide card hints

--- src/app/apps/Learn/learn-item-details/learn-item-details-hints/learn-item-details-hints.component.ts
+++ src/app/apps/Learn/learn-item-details/learn-item-details-hints/learn-item-details-hints.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LearnItemDetailsHintsComponent implements OnInit {
 
+  showDetails = false
+
   constructor() { }
 
   ngOnInit() {}
