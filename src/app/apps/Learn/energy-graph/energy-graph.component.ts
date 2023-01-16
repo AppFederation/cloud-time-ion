83398e5d95a95e1aa9f5fb454d444005099d31a6
Karol Depka <karol-depka@users.noreply.github.com>
2023-01-16 09:02:09 +0100

    Extract energy graph component

---
+++ src/app/apps/Learn/energy-graph/energy-graph.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-energy-graph',
+  templateUrl: './energy-graph.component.html',
+  styleUrls: ['./energy-graph.component.scss'],
+})
+export class EnergyGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
