0bc15fd6d052568cdc4e99f01798558d97d9030d
Karol Depka <karol-depka@users.noreply.github.com>
2023-01-27 00:08:17 +0100

    remove unused .spec.ts files

--- src/app/apps/Learn/search-or-add-learnable-item/selection.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SelectionManager } from './SelectionManager';
-
-describe('Selection', () => {
-  it('should create an instance', () => {
-    expect(new SelectionManager()).toBeTruthy();
-  });
-});
