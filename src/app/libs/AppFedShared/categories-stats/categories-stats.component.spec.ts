223edc83c10643665086094dbf8457ca6bfc270c
Karol Depka <karol-depka@users.noreply.github.com>
2023-01-27 01:21:10 +0100

    stub: categories-stats page

---
+++ src/app/libs/AppFedShared/categories-stats/categories-stats.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { CategoriesStatsComponent } from './categories-stats.component';
+
+describe('CategoriesStatsComponent', () => {
+  let component: CategoriesStatsComponent;
+  let fixture: ComponentFixture<CategoriesStatsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CategoriesStatsComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CategoriesStatsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/libs/AppFedShared/categories-stats/categories-stats.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-stats',
+  templateUrl: './categories-stats.component.html',
+  styleUrls: ['./categories-stats.component.sass'],
+})
+export class CategoriesStatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
