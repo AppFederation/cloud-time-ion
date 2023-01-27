85b8c269befb91beea20e9dea5d39f64161a5816
Karol Depka <karol-depka@users.noreply.github.com>
2023-01-27 01:52:43 +0100

    enableAll = true/false

--- src/app/libs/AppFedShared/base.service.ts
+++ src/app/libs/AppFedShared/base.service.ts
@@ -1,57 +1,59 @@
 import { Injectable } from '@angular/core';
 import {g} from './g'
 
+export let enableAll = false
+
 export class FeatureLevelsConfig {
   // ORDER: from least mature to most
 
-  showDeprecated = false
+  showDeprecated = enableAll
 
-  showTodosNotes = false
+  showTodosNotes = enableAll
 
 
   /** mockups (/notes?) of stuff that I will prolly not be able/willing to do, unless i become like Musk ;). Still kinda motivating to push harder upon seeing this. */
-  megalomania = false
+  megalomania = enableAll
 
 
   /** stuff that is not implemented, just visual for imagination / motivation / visualization */
-  mockups = false
+  mockups = enableAll
 
   /** stuff that works or gives some everyday value but looks ugly/unprofessional/unfinished */
-  showUgly = false
+  showUgly = enableAll
 
   /** an entire feature/page/sub-product could be unfinished; or just its feature/sub-feature */
-  unfinished = false
+  unfinished = enableAll
 
   /** might be unstable for me or end user */
-  showUnstable = false
+  showUnstable = enableAll
 
   /** Prolly stable for me but might be hard to understand for end user */
-  showExperimental = false
+  showExperimental = enableAll
 
   /** better than `ugly`; works and fulfills some function, e.g. energy graph; actually worse than ugly; coz deficiencies not only in looks, but also in functionality
    * TODO split: into visual / func
    * */
-  unpolished = false
+  unpolished = enableAll
 
-  unpolishedFunctionality  = false
+  unpolishedFunctionality  = enableAll
 
-  unpolishedVisually = false
+  unpolishedVisually = enableAll
 
   // TODO/idea: alpha, beta, release-candidate
 
   /** experimental / advanced */
 
 
-  showPerformanceTracking = false
+  showPerformanceTracking = enableAll
 
   /** This is becoming similar to logger levels and logger tree */
-  showDebug = false
+  showDebug = enableAll
 
-  showAdvanced = false
+  showAdvanced = enableAll
 
   /** just an idea; more high priority than todos/notes/ideas texts display; should fix before release to end-users;
    * Can disable for demos. Should prolly be last in order of maturity, to force fixing fixmes before release. */
-  showFixmes = false
+  showFixmes = enableAll
 
 }
 
