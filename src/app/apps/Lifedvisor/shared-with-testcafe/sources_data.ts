1e48369cd23a77d9978c15e0fb758821d37c05b3
Karol Depka <karol-depka@users.noreply.github.com>
2023-07-29 01:08:19 +0200

    Add lifedvisor sources books

--- src/app/apps/Lifedvisor/shared-with-testcafe/sources_data.ts
+++ src/app/apps/Lifedvisor/shared-with-testcafe/sources_data.ts
@@ -1,5 +1,13 @@
 import {setIdsFromKeys} from '../utils/dictionary-utils';
 
 export const sources = setIdsFromKeys({
-  'The 7 Habits of Highly Effective People': {}
+  'The 7 Habits of Highly Effective People': {},
+  'Atomic Habits': {},
+  'Your Brain at Work': {},
+  'Hooked': {},
+  'Pragmatic Programmer': {},
+  'Solve for Happy': {
+    subTitle: 'Engineer Your Path to Joy',
+  },
+  //           new HintSource('https://charlesduhigg.com/books/smarter-faster-better/'),
 })
