ad635e92547a589c48429c5d6409c54417109b0b
Karol Depka <karol-depka@users.noreply.github.com>
2023-08-21 00:01:06 +0200

    Remove .spec files

--- src/app/libs/AppFedShared/tree/OdmTreeModel.spec.ts
+++
@@ -1,7 +0,0 @@
-import { OdmTreeModel } from './OdmTreeModel';
-
-describe('OdmTreeModel', () => {
-  it('should create an instance', () => {
-    expect(new OdmTreeModel()).toBeTruthy();
-  });
-});
