38be862b96c8ff5556a4e3822372195bb11e2f96
Karol Depka <karol-depka@users.noreply.github.com>
2023-08-22 04:01:45 +0200

    migrate base64 cont'd

--- src/app/apps/Learn/core/migrate-img-base64.ts
+++ src/app/apps/Learn/core/migrate-img-base64.ts
@@ -12,17 +12,49 @@ export class MigrateImgBase64Service {
     private storage: AngularFireStorage
   ) { }
 
+  private getBase64Size(base64: string): number {
+    const padding = (base64.match(/=/g) || []).length;
+    return (base64.length / 4) * 3 - padding;
+  }
+
   async processLearnItems(): Promise<void> {
-    console.log(`processLearnItems start`)
     const learnItems = await this.firestore.collection('LearnItem').get().toPromise();
     const totalDocuments = learnItems.docs.length;
-    let errorCount = 0;
-    let totalImagesReplaced = 0;
     let totalSizeInBytes = 0;
+    let totalSizeSaved = 0;
+
+    // Initial pass to count total images and their sizes
+    for (const document of learnItems.docs) {
+      const data: { [key: string]: string } = document.data() as { [key: string]: string };
+      for (const key in data) {
+        if (typeof data[key] === 'string') {
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(data[key], 'text/html');
+          const imgTags = doc.querySelectorAll('img');
+          Array.from(imgTags).forEach(img => {
+            const src = img.getAttribute('src');
+            if (src) {
+              const base64 = src.split(',')[1];
+              if (base64) {
+                totalSizeInBytes += this.getBase64Size(base64);
+              }
+            }
+          });
+        }
+      }
+    }
+
+    console.log(`Total size in bytes to process: ${totalSizeInBytes.toLocaleString()} bytes`);
+
+    let sizeProcessed = 0;
+    let errorCount = 0;
 
     for (const [index, document] of learnItems.docs.entries()) {
-      console.log(`Processing document ${index + 1} of ${totalDocuments}`); // Print the progress
+      console.log(`Processing document ${index + 1} of ${totalDocuments}`);
+      console.log(`Progress: Bytes ${sizeProcessed.toLocaleString()} of ${totalSizeInBytes.toLocaleString()} (${((sizeProcessed / totalSizeInBytes) * 100).toFixed(2)}%)`);
+
       try {
+        let documentChanged = false;
         const data: { [key: string]: string } = document.data() as { [key: string]: string };
         for (const key in data) {
           if (typeof data[key] === 'string') {
@@ -31,38 +63,46 @@ export class MigrateImgBase64Service {
             const imgTags = doc.querySelectorAll('img');
             await Promise.all(Array.from(imgTags).map(async (img, imgIndex) => {
               const src = img.getAttribute('src');
-              const match = src?.match(/^data:image\/(.*?);base64,/);
-              if (match) {
-                const base64 = src?.split(',')[1];
-                if (base64) {
-                  const sizeInBytes = Buffer.from(base64, 'base64').length;
-                  totalSizeInBytes += sizeInBytes;
+              if (src) {
+                const match = src.match(/^data:image\/(.*?);base64,/);
+                if (match) {
+                  const base64 = src.split(',')[1];
+                  if (base64) {
+                    const sizeInBytes = this.getBase64Size(base64);
+                    sizeProcessed += sizeInBytes;
+                    totalSizeSaved += sizeInBytes;
 
-                  const extension = match[1];
-                  const filePath = `images/${document.id}_${key}_${imgIndex}.${extension}`;
-                  const fileRef = this.storage.ref(filePath);
-                  // await this.storage.upload(filePath, base64); // FIXME
-                  // const url = await fileRef.getDownloadURL().toPromise();
-                  const url = 'ZZZZ FIXME'
-                  console.log(`Image replaced in document ${document.id}, field ${key}, index ${imgIndex}: ${src} -> ${url}`);
-                  img.setAttribute('src', url);
+                    const extension = match[1];
+                    // const filePath = `images/${document.id}_${key}_${imgIndex}.${extension}`;
+                    // const fileRef = this.storage.ref(filePath);
+                    // await this.storage.upload(filePath, base64);
+                    // const url = await fileRef.getDownloadURL().toPromise();
+                    const url = 'FIXME ZZZ';
+                    img.setAttribute('src', url);
 
-                  totalImagesReplaced++;
+                    documentChanged = true;
+                  }
                 }
               }
             }));
-            data[key] = doc.body.innerHTML;
+            if (documentChanged) {
+              data[key] = doc.body.innerHTML;
+            }
           }
         }
-        await this.firestore.collection('LearnItem_Processed').doc(document.id).set(data);
+
+        if (documentChanged) {
+          await this.firestore.collection('LearnItem_Processed').doc(document.id).set(data);
+        }
+
         console.log(`Document ${document.id} processed successfully. No errors.`);
       } catch (error) {
         console.error(`An error occurred while processing document ${document.id}:`, error);
         errorCount++;
       }
     }
+
     console.log(`Processing complete. Total errors: ${errorCount}`);
-    console.log(`Total images replaced: ${totalImagesReplaced}`);
-    console.log(`Total size in bytes: ${totalSizeInBytes}`);
+    console.log(`Total size in bytes saved: ${totalSizeSaved.toLocaleString()} bytes`);
   }
 }
