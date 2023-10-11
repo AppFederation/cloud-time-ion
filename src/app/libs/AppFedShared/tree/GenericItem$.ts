2bdd94b30874c67f2f034ecd62a124c7b7d678f7
Karol Depka <karol-depka@users.noreply.github.com>
2023-10-12 01:58:11 +0200

    ChatGPTCommit: Add comment in GenericItem$

--- src/app/libs/AppFedShared/tree/GenericItem$.ts
+++ src/app/libs/AppFedShared/tree/GenericItem$.ts
@@ -2,6 +2,7 @@ import {OdmItem$2} from '../../../libs/AppFedShared/odm/OdmItem$2'
 import {GenericItemsService} from './generic-items.service'
 import {GenericItem} from './GenericItem'
 
+/** TODO merge with class DbItem<TData = any> from OrYoL */
 export class GenericItem$
   extends OdmItem$2<
     GenericItem$,
