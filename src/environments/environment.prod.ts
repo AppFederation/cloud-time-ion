6482dd2c3217d9c6b69c285816211853912e0d77
Karol Depka <karol-depka@users.noreply.github.com>
2023-10-12 23:44:17 +0200

    add environmentBase for .prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+import {environmentBase} from './environment.base'
+
 export const environment = {
+  ... environmentBase,
+  collectionNameSuffix: '' /* NOTE: never put debug suffix here, coz wanna deploy to vercel to use it */,
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,27 +1,7 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import {environmentBase} from './environment.base'
 
 export const environment = {
-  collectionNameSuffix: '',
-  // collectionNameSuffix: '_DEBUG',
-  showExperimentalThemes: true,
-  firebaseConfig: {
-    projectId: 'cloudtime-app',
-    apiKey: "AIzaSyD8hiBc7WoQQISCDpDLMtiaakyKvKZwdkw",
-    authDomain: "cloudtime-app.firebaseapp.com",
-    databaseURL: "https://cloudtime-app.firebaseio.com",
-    storageBucket: "cloudtime-app.appspot.com",
-    // messagingSenderId: "42917465053"
-  },
-  production: true
+  ... environmentBase,
+  // collectionNameSuffix: ''
+  // production: true
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
