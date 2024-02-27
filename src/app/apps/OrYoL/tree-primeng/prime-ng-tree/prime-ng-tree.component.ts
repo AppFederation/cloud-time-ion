9a7082ff3582d3648bbb9f4ba41019929b5fda4e
Karol Depka <karol-depka@users.noreply.github.com>
2024-02-27 12:55:40 +0100

    `nodeDrop(event: any) {` -- primeng 15

--- src/app/apps/OrYoL/tree-primeng/prime-ng-tree/prime-ng-tree.component.ts
+++ src/app/apps/OrYoL/tree-primeng/prime-ng-tree/prime-ng-tree.component.ts
@@ -1,7 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {TreeModel} from '../../tree-model/TreeModel'
 import {TreeHostComponent} from '../../tree-host/tree-host/tree-host.component'
-import {TreeNodeDropEvent} from 'primeng/tree'
 
 @Component({
   selector: 'app-prime-ng-tree',
@@ -21,7 +20,7 @@ export class PrimeNgTreeComponent implements OnInit {
   ngOnInit() {
   }
 
-  nodeDrop(event: TreeNodeDropEvent) {
+  nodeDrop(event: any) {
     console.log('nodeDrop', event)
     // this.dbService.moveNode(event.dragNode.dbId, event.dropNode.dbId) // FIXME
   }
