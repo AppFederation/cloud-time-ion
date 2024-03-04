14d894ac78441c2ef9b35df002e0aa2b19a284e0
Karol Depka <karol-depka@users.noreply.github.com>
2024-03-04 04:32:10 +0100

    tried rxjs 7 upgrade - problem with CachedSubject2

--- src/app/libs/AppFedShared/utils/cachedSubject2/CachedSubject2.ts
+++ src/app/libs/AppFedShared/utils/cachedSubject2/CachedSubject2.ts
@@ -1,5 +1,6 @@
 import {Subject, Subscriber, Subscription} from 'rxjs';
 import {isNotNullish} from '../utils'
+import {BehaviorSubject} from 'rxjs/internal/BehaviorSubject'
 
 /** Similar to ReplaySubject<T>, but storing only 1 previous cached value.
  * This version is implemented without using ReplaySubject internally.
@@ -11,12 +12,17 @@ export class CachedSubject<T> extends Subject<T> {
 
   public lastVal ? : T
 
+  // get lastVal() {
+  //   return this.value
+  // }
+
   /** Useful for e.g. determining in UI if initial data set has arrived (distinguish from initial empty) */
   public hasEmitted = false
 
   private onSubscribeCallBack ? : (subscriber: Subscriber<T>) => void
 
-  constructor(initialVal ? : T) {
+  constructor(initialVal: T) {
+    // super(initialVal)
     super()
     if ( initialVal !== undefined ) {
       this.next(initialVal)
@@ -34,8 +40,8 @@ export class CachedSubject<T> extends Subject<T> {
     return this.next(val)
   }
 
-  public override _subscribe(subscriber: Subscriber<T>): Subscription {
-    const subscription = super._subscribe(subscriber);
+  public override subscribe(subscriber: Subscriber<T>): Subscription {
+    const subscription = super.subscribe(subscriber);
     if ( this.hasEmitted ) {
       subscriber.next(this.lastVal)
     }
